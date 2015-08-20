56990ebf689fd28990f421e81f5a88aa94c98aff
Minyoung Choi <minchoi@stanford.edu>
2015-08-20 09:41:23 -0700

    disable link

--- src/clincoded/static/components/dashboard.js
+++ src/clincoded/static/components/dashboard.js
@@ -148,7 +148,7 @@ var Dashboard = React.createClass({
                             <h3>Tools</h3>
                             <ul>
                                 <li><a href="/create-gene-disease/">Create Gene-Disease Record</a></li>
-                                <li><a href="/gdm/">View list of all Gene-Disease Records</a></li>
+                                <li>View list of all Gene-Disease Records</li>
                             </ul>
                         </Panel>
                         <Panel panelClassName="panel-dashboard">
