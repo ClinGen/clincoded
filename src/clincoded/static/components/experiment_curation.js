79f677962e418d173dd5ac5e00a48746a693b33f
Minyoung Choi <minchoi@stanford.edu>
2015-08-25 11:46:16 -0700

    basic setup for experiment curation page

---
+++ src/clincoded/static/components/experiment_curation.js
@@ -0,0 +1,29 @@
+'use strict';
+var React = require('react');
+var url = require('url');
+var _ = require('underscore');
+var moment = require('moment');
+var panel = require('../libs/bootstrap/panel');
+var form = require('../libs/bootstrap/form');
+var globals = require('./globals');
+var curator = require('./curator');
+var RestMixin = require('./rest').RestMixin;
+var methods = require('./methods');
+
+var CurationMixin = curator.CurationMixin;
+var RecordHeader = curator.RecordHeader;
+var CurationPalette = curator.CurationPalette;
+var PmidSummary = curator.PmidSummary;
+var PanelGroup = panel.PanelGroup;
+var Panel = panel.Panel;
+var Form = form.Form;
+var FormMixin = form.FormMixin;
+var Input = form.Input;
+var InputMixin = form.InputMixin;
+var PmidDoiButtons = curator.PmidDoiButtons;
+var queryKeyValue = globals.queryKeyValue;
+var country_codes = globals.country_codes;
+
+
+var GroupCuration = React.createClass({
+});
