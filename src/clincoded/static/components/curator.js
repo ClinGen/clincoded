720fdfe91da4ea0ddc995f3025f9fa7a8ff3343c
Forrest Tanaka <forrest@forrest-tanaka.com>
2015-09-25 17:05:26 -0700

    In progress

--- src/clincoded/static/components/curator.js
+++ src/clincoded/static/components/curator.js
@@ -131,19 +131,23 @@ var VariantHeader = module.exports.VariantHeader = React.createClass({
                             var userPathogenicity = null;
 
                             // See if the variant has a pathogenicity curated in the current GDM
+                            var matchingPathogenicity;
                             var inCurrentGdm = _(variant.associatedPathogenicities).find(function(pathogenicity) {
                                 var matchingGdm = _(pathogenicity.associatedGdm).find(function(associatedGdm) {
                                     return associatedGdm.uuid === gdm.uuid;
                                 });
+                                if (matchingGdm) {
+                                    matchingPathogenicity = pathogenicity;
+                                }
                                 return !!matchingGdm;
                             });
 
-                            if (session) {
+                            if (session && inCurrentGdm) {
                                 userPathogenicity = getPathogenicityFromVariant(variant, session.user_properties.uuid);
                             }
                             return (
                                 <div className="col-sm-6 col-md-3 col-lg-2" key={variant.uuid}>
-                                    <a className="btn btn-primary btn-xs" href={'/variant-curation/?all&gdm=' + gdm.uuid + (pmid ? '&pmid=' + pmid : '') + '&variant=' + variant.uuid + (session ? '&user=' + session.user_properties.uuid : '') + (userPathogenicity ? '&pathogenicity=' + userPathogenicity.uuid : '')}>
+                                    <a className="btn btn-primary btn-xs" href={'/variant-curation/?all&gdm=' + gdm.uuid + (pmid ? '&pmid=' + pmid : '') + '&variant=' + variant.uuid + (session ? '&user=' + session.user_properties.uuid : '') + (matchingPathogenicity ? '&pathogenicity=' + matchingPathogenicity.uuid : '')}>
                                         {inCurrentGdm ? <i className="icon icon-sticky-note"></i> : null}
                                         {variantName}
                                     </a>
