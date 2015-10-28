862ea3bdf6ecfb803b59adfe95c7ef12e909fb15
Minyoung Choi <minchoi@stanford.edu>
2015-10-28 14:57:49 -0700

    clinvar linkout on variant curation page

--- src/clincoded/static/components/variant_curation.js
+++ src/clincoded/static/components/variant_curation.js
@@ -29,6 +29,7 @@ var PmidDoiButtons = curator.PmidDoiButtons;
 var queryKeyValue = globals.queryKeyValue;
 var country_codes = globals.country_codes;
 var truncateString = globals.truncateString;
+var external_url_map = globals.external_url_map;
 
 
 var VariantCuration = React.createClass({
@@ -376,7 +377,7 @@ var VariantCuration = React.createClass({
                     <div className="viewer-titles">
                         <h1>{(pathogenicity ? 'Edit' : 'Curate') + ' Variant Information'}</h1>
                         {variant ?
-                            <h2>{variant.clinvarVariantId ? <span>{'VariationId: ' + variant.clinvarVariantId}</span> : <span>{'Description: ' + variant.otherDescription}</span>}</h2>
+                            <h2>{variant.clinvarVariantId ? <span><a href={external_url_map['ClinVarSearch'] + variant.clinvarVariantId} title={"ClinVar entry for variant " + variant.clinvarVariantId + " in new tab"} target="_blank">{variant.clinvarVariantId}</a></span> : <span>{'Description: ' + variant.otherDescription}</span>}</h2>
                         : null}
                         {curatorName ? <h2>{'Curator: ' + curatorName}</h2> : null}
                     </div>
