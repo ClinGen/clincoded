a1b68a6939321d9220a717414ad4300c43406185
Philip_Tzou <philip.npc@gmail.com>
2015-11-07 19:10:29 -0800

    Reload support and restart support; update document

--- docker-enterpoint.sh
+++ docker-enterpoint.sh
@@ -12,13 +12,16 @@ cat > ~/.npmrc << EOF
 unsafe-perm = true
 EOF
 pushd /code > /dev/null
-if [ ! -f /.buildout_done ]; then
+if [ ! -f /vendor/.buildout_done ]; then
     bin/buildout -c $1
     INITARGS="--clear --init --load"
-    touch /.buildout_done
+    touch /vendor/.buildout_done
+    cp -r /code/{ontology.json,.installed.cfg,.mr.developer.cfg} /vendor
+else
+    bin/buildout -c $1 install clincoded
 fi
 mkdir -p /data/clincoded
 chmod 777 /data/clincoded
+rm -rf /tmp/clincoded
 ln -s /data/clincoded /tmp/clincoded
-sleep 86400000
-# PATH=$PATH:/usr/lib/postgresql/9.4/bin:/usr/share/elasticsearch/bin bin/dev-servers development.ini --app-name app $INITARGS & while ! nc -z 127.0.0.1 5432 && ! nc -z 127.0.0.1 9200; do sleep 5; done; bin/pserve development.ini
+PATH=$PATH:/usr/lib/postgresql/9.4/bin:/usr/share/elasticsearch/bin bin/dev-servers development.ini --app-name app $INITARGS & while ! nc -z 127.0.0.1 5432 && ! nc -z 127.0.0.1 9200; do sleep 5; done; while bin/grunt watch; sleep 5; do echo; done & while bin/compass watch; sleep 5; do echo; done & bin/pserve development.ini --reload
