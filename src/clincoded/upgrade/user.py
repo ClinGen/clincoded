9dc792a8c411465f327fae28040baa4f5f9ff4fd
Minyoung Choi <minchoi@stanford.edu>
2015-12-08 17:02:17 -0800

    change user upgrade + test

--- src/clincoded/upgrade/user.py
+++ src/clincoded/upgrade/user.py
@@ -11,14 +11,14 @@ def user_2_3(value, system):
 @upgrade_step('user', '3', '4')
 def user_3_4(value, system):
     # https://github.com/ClinGen/clincoded/issues/453
-    if value['uuid'] not in [
-        '627eedbc-7cb3-4de3-9743-a86266e435a6',  # Forest
-        '870b7c6d-76f2-4c19-8393-e2f1398cfd99',  # Karen
-        'e49d01a5-51f7-4a32-ba0e-b2a71684e4aa',  # Kang
-        '04442cec-6ab7-40d0-af26-1e5fefd535eb',  # Selina
-        '3a70a47f-711a-473f-93cd-0996421eeaf2',  # Minyoung
-        '748c68cb-b59e-48dd-88e9-1bcd8be199d6',  # Ben
-        '5a97bb8e-8b87-4863-91c1-98080d46d4be',  # Mike
-        'c0a70214-08a8-48a0-ba8d-521bed321d4a',  # Matt
+    if value['email'] not in [
+        'fytanaka@stanford.edu',  # Forest
+        'kdalton@stanford.edu',  # Karen
+        'kgliu@stanford.edu',  # Kang
+        'selinad@stanford.edu',  # Selina
+        'minchoi@stanford.edu',  # Minyoung
+        'hitz@stanford.edu',  # Ben
+        'cherry@stanford.edu',  # Mike
+        'wrightmw@stanford.edu',  # Matt
     ]:
         value['groups'] = ['curator']
