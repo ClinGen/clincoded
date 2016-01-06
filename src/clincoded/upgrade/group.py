bd65191d5bb8362df0dee187a7c1b90125f19e87
Minyoung Choi <minchoi@stanford.edu>
2016-01-06 11:22:09 -0800

    schema upgrade scripts

--- src/clincoded/upgrade/group.py
+++ src/clincoded/upgrade/group.py
@@ -5,3 +5,11 @@ from contentbase.upgrader import upgrade_step
 def group_1_2(value, system):
     # https://github.com/ClinGen/clincoded/issues/453
     value['status'] = 'in progress'
+
+@upgrade_step('group', '2', '3')
+def group_2_3(value, system):
+    # https://github.com/ClinGen/clincoded/issues/401
+    if 'method' in value:
+        if 'genotypingMethods' in value['method']:
+            value['method']['genotypingMethods'] = [e.replace('Sanger', 'Sanger sequencing') for e in value['methods']['genotypingMethods']]
+            value['method']['genotypingMethods'] = [e.replace('HRM', 'High resolution melting') for e in value['methods']['genotypingMethods']]
