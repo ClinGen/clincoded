07422bb8c58128b728fea53d61b6656c616bfc85
Minyoung Choi <minchoi@stanford.edu>
2016-01-06 15:35:26 -0800

    fix typos in schema upgrade scripts

--- src/clincoded/upgrade/family.py
+++ src/clincoded/upgrade/family.py
@@ -11,5 +11,5 @@ def family_2_3(value, system):
     # https://github.com/ClinGen/clincoded/issues/401
     if 'method' in value:
         if 'genotypingMethods' in value['method']:
-            value['method']['genotypingMethods'] = [e.replace('Sanger', 'Sanger sequencing') for e in value['methods']['genotypingMethods']]
-            value['method']['genotypingMethods'] = [e.replace('HRM', 'High resolution melting') for e in value['methods']['genotypingMethods']]
+            value['method']['genotypingMethods'] = [e.replace('Sanger', 'Sanger sequencing') for e in value['method']['genotypingMethods']]
+            value['method']['genotypingMethods'] = [e.replace('HRM', 'High resolution melting') for e in value['method']['genotypingMethods']]
--- src/clincoded/upgrade/individual.py
+++ src/clincoded/upgrade/individual.py
@@ -6,10 +6,11 @@ def individual_1_2(value, system):
     # https://github.com/ClinGen/clincoded/issues/453
     value['status'] = 'in progress'
 
+
 @upgrade_step('individual', '2', '3')
 def individual_2_3(value, system):
     # https://github.com/ClinGen/clincoded/issues/401
     if 'method' in value:
         if 'genotypingMethods' in value['method']:
-            value['method']['genotypingMethods'] = [e.replace('Sanger', 'Sanger sequencing') for e in value['methods']['genotypingMethods']]
-            value['method']['genotypingMethods'] = [e.replace('HRM', 'High resolution melting') for e in value['methods']['genotypingMethods']]
+            value['method']['genotypingMethods'] = [e.replace('Sanger', 'Sanger sequencing') for e in value['method']['genotypingMethods']]
+            value['method']['genotypingMethods'] = [e.replace('HRM', 'High resolution melting') for e in value['method']['genotypingMethods']]
