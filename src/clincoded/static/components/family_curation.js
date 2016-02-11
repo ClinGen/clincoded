c1408c226e5261392691820fcbf04e03d9320ac8
Jimmy Zhen <jzhen@stanford.edu>
2016-02-10 16:21:28 -0800

    Added warning note to Individual Label input field on family curation page

--- src/clincoded/static/components/family_curation.js
+++ src/clincoded/static/components/family_curation.js
@@ -1674,6 +1674,7 @@ var FamilyVariant = function() {
                     <Input type="text" ref="individualname" label="Individual Label"
                         error={this.getFormError('individualname')} clearError={this.clrFormErrors.bind(null, 'individualname')}
                         labelClassName="col-sm-5 control-label" wrapperClassName="col-sm-7" groupClassName="form-group" required />
+                    <p className="col-sm-7 col-sm-offset-5 input-note-below">Note: Do not enter real names in this field. {curator.renderLabelNote('Individual')}</p>
                     {this.state.orpha ?
                         <div className="form-group">
                             <div className="col-sm-5"><strong className="pull-right">Orphanet Disease(s) Associated with Family:</strong></div>
