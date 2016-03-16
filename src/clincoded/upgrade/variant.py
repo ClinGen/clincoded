e45d5784d981f065fe18274d1a2e26ca95975a89
Minyoung Choi <minchoi@stanford.edu>
2016-03-16 14:44:11 -0700

    python3

--- src/clincoded/upgrade/variant.py
+++ src/clincoded/upgrade/variant.py
@@ -1,5 +1,5 @@
 from contentbase.upgrader import upgrade_step
-import urllib
+import urllib.request
 import json
 
 @upgrade_step('variant', '1', '2')
@@ -7,8 +7,9 @@ def variant_1_2(value, system):
     # for adding optional field clinvarVariantTitle
     if 'clinvarVariantTitle' not in value:
         if 'clinvarVariantId' in value:
-            response = urllib.urlopen('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=clinvar&id=%s&retmode=json' % value['clinvarVariantId'])
-            data = json.loads(response.read())
+            response = urllib.request.urlopen('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=clinvar&id=%s&retmode=json' % value['clinvarVariantId'])
+            str_response = response.readall().decode('utf-8')
+            data = json.loads(str_response)
             value['clinvarVariantTitle'] = str(data['result'][value['clinvarVariantId']]['title'])
         else:
             value['clinvarVariantTitle'] = ''
