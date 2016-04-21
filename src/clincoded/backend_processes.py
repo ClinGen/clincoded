54babae34d0c7d715012f731cc46b2ae5ab4c9d0
Minyoung Choi <minchoi@stanford.edu>
2016-04-21 11:22:38 -0700

    expand traverse

--- src/clincoded/backend_processes.py
+++ src/clincoded/backend_processes.py
@@ -13,13 +13,45 @@ def includeme(config):
     config.add_route('traverse', '/traverse/{gdm}')
     config.scan(__name__)
 
-@view_config(route_name='traverse')
+
+def traverse_action(request, obj):
+    aid = obj['@id']
+    if 'label' in obj:
+        label = obj['label']
+        print('%s : %s' % (aid, label))
+    else:
+        print('%s' % aid)
+    root = request.embed('%s' % aid, as_user=True)
+    # print(root)
+    if 'annotations' in root:
+        for annotation in root['annotations']:
+            traverse_action(request, annotation)
+    if 'groups' in root:
+        for group in root['groups']:
+            traverse_action(request, group)
+    if 'families' in root:
+        for family in root['families']:
+            traverse_action(request, family)
+    if 'individuals' in root:
+        for individual in root['individuals']:
+            traverse_action(request, individual)
+    if 'familyIncluded' in root:
+        for family in root['familyIncluded']:
+            traverse_action(request, family)
+    if 'individualIncluded' in root:
+        for individual in root['individualIncluded']:
+            traverse_action(request, individual)
+    if 'experimentalData' in root:
+        for experimentalDatum in root['experimentalData']:
+            traverse_action(request, experimentalDatum)
+
+
+@view_config(route_name='traverse', request_method='GET', permission='view')
 def traverse(context, request):
     #param_list = parse_qs(request.matchdict['gdm'].replace(',,', '&'))
     gdm = request.matchdict['gdm']
-    results = request.embed('/gdm/%s' % gdm, as_user=True)['@graph']
+    root = request.embed('/gdm/%s' % gdm, as_user=True)
     print("\n****************************************\n")
-    print(gdm)
-    print(results)
+    traverse_action(request, root)
     print("\n****************************************\n")
-    return False
+    return Response(False)
