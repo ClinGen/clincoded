92e129285945b1289828bd2f1d4e45764ffec330
Minyoung Choi <minchoi@stanford.edu>
2016-04-22 14:26:46 -0700

    expand traverse function; get response to return json

--- src/clincoded/backend_processes.py
+++ src/clincoded/backend_processes.py
@@ -7,6 +7,7 @@ from urllib.parse import (
     parse_qs,
     urlencode,
 )
+from json import dumps
 
 
 def includeme(config):
@@ -15,35 +16,46 @@ def includeme(config):
 
 
 def traverse_action(request, obj):
+    payload = []
     aid = obj['@id']
     if 'label' in obj:
         label = obj['label']
         print('%s : %s' % (aid, label))
+        payload += [{
+            'label': obj['label'],
+            'id': aid
+        }]
     else:
         print('%s' % aid)
+        payload += [{
+            'id': aid
+        }]
+
     root = request.embed('%s' % aid, as_user=True)
     # print(root)
     if 'annotations' in root:
         for annotation in root['annotations']:
-            traverse_action(request, annotation)
+            payload += traverse_action(request, annotation)
     if 'groups' in root:
         for group in root['groups']:
-            traverse_action(request, group)
+            payload += traverse_action(request, group)
     if 'families' in root:
         for family in root['families']:
-            traverse_action(request, family)
+            payload += traverse_action(request, family)
     if 'individuals' in root:
         for individual in root['individuals']:
-            traverse_action(request, individual)
+            payload += traverse_action(request, individual)
     if 'familyIncluded' in root:
         for family in root['familyIncluded']:
-            traverse_action(request, family)
+            payload += traverse_action(request, family)
     if 'individualIncluded' in root:
         for individual in root['individualIncluded']:
-            traverse_action(request, individual)
+            payload += traverse_action(request, individual)
     if 'experimentalData' in root:
         for experimentalDatum in root['experimentalData']:
-            traverse_action(request, experimentalDatum)
+            payload += traverse_action(request, experimentalDatum)
+
+    return payload
 
 
 @view_config(route_name='traverse', request_method='GET', permission='view')
@@ -52,6 +64,8 @@ def traverse(context, request):
     gdm = request.matchdict['gdm']
     root = request.embed('/gdm/%s' % gdm, as_user=True)
     print("\n****************************************\n")
-    traverse_action(request, root)
+    payload = traverse_action(request, root)
     print("\n****************************************\n")
-    return Response(False)
+    print(payload)
+    print("\n****************************************\n")
+    return Response(dumps(payload), content_type='text/plain')
