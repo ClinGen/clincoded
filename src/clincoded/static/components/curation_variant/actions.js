5b97b7fc72298b98936d7d730dd64fd1c54d8773
Jimmy Zhen <jzhen@stanford.edu>
2016-05-20 12:36:59 -0700

    Updated code to handle UI display with location.href instead of context.navigate, as well as to create a new interpretation object given the proper handler.

--- src/clincoded/static/components/curation_variant/actions.js
+++ src/clincoded/static/components/curation_variant/actions.js
@@ -1,44 +1,59 @@
 'use strict';
 var React = require('react');
 var globals = require('../globals');
+var RestMixin = require('../rest').RestMixin;
+var parseAndLogError = require('../mixins').parseAndLogError;
 var form = require('../../libs/bootstrap/form');
 
 var Input = form.Input;
 var queryKeyValue = globals.queryKeyValue;
 
-// Display the curator data of the curation data
+// Display the variant curation action bar above the criteria and tabs
 var VariantCurationActions = module.exports.VariantCurationActions = React.createClass({
+    mixins: [RestMixin],
+
     propTypes: {
         variantData: React.PropTypes.object, // ClinVar data payload
-        session: React.PropTypes.object
-    },
-
-    contextTypes: {
-        navigate: React.PropTypes.func
+        session: React.PropTypes.object,
+        interpretationUuid: React.PropTypes.string,
     },
 
     getInitialState: function() {
         return {
-            interpretationUuid: queryKeyValue('interpretation', this.props.href)
+            variantUuid: null,
+            interpretationUuid: null
         };
     },
-
+    /*
+    componentDidUpdate: function(prevProps, prevState) {
+        if (prevState.interpretationUuid !== this.state.interpretationUuid && this.state.interpretationUuid && this.state.variantUuid) {
+            this.context.navigate('/curation-variant/?variant=' + this.state.variantUuid + '&interpretation=' + this.state.interpretationUuid);
+        } else {
+            return false;
+        }
+    },
+    */
+    // handler for 'Start new interpreation' button click event
     handleNewInterpretation: function(e) {
         e.preventDefault(); e.stopPropagation();
-        var variant = this.props.variantData;
-        if (variant) {
-            var variantUuid = (variant.uuid) ? variant.uuid : 'Unknown';
+        var variantObj = this.props.variantData;
+        var newInterpretationObj;
+        if (variantObj) {
+            this.setState({variantUuid: variantObj.uuid});
+            // Put together a new interpretation object
+            newInterpretationObj = {variant: variantObj.uuid};
         }
-        //window.history.replaceState(window.state, '', '/curation-variant/?variant=' + variantUuid + '&interpretation=' + 'f26a694d-5f09-45fd-818a-861e4108e65a');
-        this.context.navigate('/curation-variant/?variant=' + variantUuid + '&interpretation=' + 'f26a694d-5f09-45fd-818a-861e4108e65a');
+        // Post new interpretation to the DB. Once promise returns, go to /curation-variant page with
+        // the new interpretation UUID in the query string.
+        this.postRestData('/interpretations/', newInterpretationObj).then(data => {
+            var newInterpretationUuid = data['@graph'][0].uuid;
+            // this.setState({interpretationUuid: newInterpretationUuid});
+            window.location.href = '/variant-central/?variant=' + this.state.variantUuid + '&interpretation=' + newInterpretationUuid;
+        }).catch(e => {parseAndLogError.bind(undefined, 'postRequest')});
     },
 
     render: function() {
         /*
-        var variant = this.props.data;
-        if (variant) {
-            var variantUuid = (variant.uuid) ? variant.uuid : 'Unknown';
-        }
         var session = this.props.session;
         if (session) {
             var userId = (session.user_properties.uuid) ? session.user_properties.uuid : '';
@@ -47,11 +62,11 @@ var VariantCurationActions = module.exports.VariantCurationActions = React.creat
         return (
             <div>
                 <div className="container curation-actions curation-variant">
-                {(this.state.interpretationUuid && this.state.interpretationUuid.length > 0) ?
+                {(this.props.interpretationUuid && this.props.interpretationUuid.length > 0) ?
                     <div className="interpretation-record clearfix">
-                        <Input type="button-button" inputClassName="btn-primary pull-left" title="Return to evidence-only" />
-                        <h3><span>Variant Interpretation Record</span></h3>
-                        <Input type="button-button" inputClassName="btn-primary pull-right" title="Associate disease" />
+                        <Input type="button-button" inputClassName="btn-primary pull-left" title="Return to Evidence Only" />
+                        <h2><span>Variant Interpretation Record</span></h2>
+                        <Input type="button-button" inputClassName="btn-primary pull-right" title="Associate with Disease" />
                     </div>
                     :
                     <div className="evidence-only clearfix"><Input type="button-button" inputClassName="btn-primary pull-right" title="Start new interpretation" clickHandler={this.handleNewInterpretation} /></div>
