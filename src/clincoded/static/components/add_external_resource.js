98f0c88c6e985f48a969750b2cf1909f69218409
Minyoung Choi <minchoi@stanford.edu>
2016-05-31 09:04:55 -0700

    switch over CAR resource URLs to production

--- src/clincoded/static/components/add_external_resource.js
+++ src/clincoded/static/components/add_external_resource.js
@@ -472,7 +472,7 @@ function carQueryResource() {
     this.saveFormValue('resourceId', this.state.inputValue);
     var error_msg;
     if (carValidateForm.call(this)) {
-        var url = 'http://reg.test.genome.network/allele/';
+        var url = 'http://reg.genome.network/allele/';
         var data;
         var id = this.state.inputValue;
         this.getRestData(url + id).then(json => {
