38b76363ca8c97e9e61bf82e4d890352cd3afc63
Minyoung Choi <minchoi@stanford.edu>
2016-06-20 12:40:27 -0700

    implement checkboxes to forms

--- src/clincoded/static/components/variant_central/interpretation/computational.js
+++ src/clincoded/static/components/variant_central/interpretation/computational.js
@@ -4,19 +4,35 @@ var _ = require('underscore');
 var moment = require('moment');
 var globals = require('../../globals');
 var RestMixin = require('../../rest').RestMixin;
+var CurationInterpretationForm = require('./shared/form').CurationInterpretationForm;
+
+var form = require('../../../libs/bootstrap/form');
+
+var Form = form.Form;
+var FormMixin = form.FormMixin;
+var Input = form.Input;
+var InputMixin = form.InputMixin;
 
 // Display the curator data of the curation data
 var CurationInterpretationComputational = module.exports.CurationInterpretationComputational = React.createClass({
     mixins: [RestMixin],
 
     propTypes: {
-        data: React.PropTypes.object
+        data: React.PropTypes.object, // ClinVar data payload
+        interpretation: React.PropTypes.object,
+        shouldFetchData: React.PropTypes.bool,
+        updateInterpretationObj: React.PropTypes.func
     },
 
     getInitialState: function() {
         return {
-            clinvar_id: null
+            clinvar_id: null,
+            interpretation: this.props.interpretation
         };
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        this.setState({interpretation: nextProps.interpretation});
     },
 
     render: function() {
@@ -30,7 +46,44 @@ var CurationInterpretationComputational = module.exports.CurationInterpretationC
                         </div>
                     </li>
                 </ul>
+
+                {(this.state.interpretation) ?
+                <div className="row group-curation-content">
+                    <div className="col-sm-12">
+                        <CurationInterpretationForm formTitle={"Criteria Group Xbox"} renderedFormContent={comp_crit_1}
+                            evidenceType={'computational'} evidenceData={this.state.data} evidenceDataUpdated={true}
+                            formDataUpdater={comp_crit_1_update} variantUuid={this.props.data['@id']} criteria={['xbox1']}
+                            interpretation={this.state.interpretation} updateInterpretationObj={this.props.updateInterpretationObj} />
+                    </div>
+                </div>
+                : null}
             </div>
         );
     }
 });
+
+var comp_crit_1 = function() {
+    return (
+        <div>
+            <Input type="checkbox" ref="xbox1-value" label="Xbox1?:"
+                labelClassName="col-sm-5 control-label" wrapperClassName="col-sm-7" groupClassName="form-group" />
+            }
+            <Input type="checkbox" ref="xbox2-value" label="Xbox2?:"
+                labelClassName="col-sm-5 control-label" wrapperClassName="col-sm-7" groupClassName="form-group" />
+        </div>
+    );
+};
+
+var comp_crit_1_update = function(nextProps) {
+    if (nextProps.interpretation) {
+        if (nextProps.interpretation.evaluations && nextProps.interpretation.evaluations.length > 0) {
+            nextProps.interpretation.evaluations.map(evaulation => {
+                if (evaulation.criteria == 'pm2') {
+                    this.refs['pm2-value'].setValue(evaulation.value);
+                    this.refs['pm2-description'].setValue(evaulation.description);
+                    this.setState({submitDisabled: false});
+                }
+            });
+        }
+    }
+};
