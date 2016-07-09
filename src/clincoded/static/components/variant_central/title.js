4ea10cfdd8872decd5d380e52c9948e84fbd655e
Kang Liu <kgliu0101@gmail.com>
2016-07-08 18:16:29 -0700

    GRCh38

--- src/clincoded/static/components/variant_central/title.js
+++ src/clincoded/static/components/variant_central/title.js
@@ -9,14 +9,20 @@ var Title = module.exports.Title = React.createClass({
 
     render: function() {
         var variant = this.props.data;
+        var variantTitle = (variant && variant.clinvarVariantTitle) ? variant.clinvarVariantTitle : null;
+        if (variant && !variantTitle && variant.hgvsNames && variant.hgvsNames != {}) {
+            variantTitle = variant.hgvsNames.GRCh38 ? variant.hgvsNames.GRCh38+' (GRCh38)': (variant.hgvsNames.GRCh37 ? variant.hgvsNames.GRCh37+' (GRCh37)' : variant.hgvsNames.others[0]);
+        } else if (!variantTitle) {
+            variantTitle = 'A preferred title is not available';
+        }
+
         if (variant) {
-            var clinVarTitle = (variant.clinvarVariantTitle) ? variant.clinvarVariantTitle : 'A preferred title is not available';
             var associatedDisease = (variant.disease) ? variant.disease : 'Not yet associated with a disease';
         }
 
         return (
             <div>
-                <h1>{clinVarTitle}{this.props.children}</h1>
+                <h1>{variantTitle}{this.props.children}</h1>
                 <h2>{associatedDisease}</h2>
             </div>
         );
