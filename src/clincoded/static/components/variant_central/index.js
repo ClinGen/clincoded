661a1794471d640f5fc1dd00eabd6bc39ef305a4
Jimmy Zhen <jzhen@stanford.edu>
2016-07-10 00:50:12 -0700

    Fixed typo in 'editKey' props; changed error msg text.

--- src/clincoded/static/components/variant_central/index.js
+++ src/clincoded/static/components/variant_central/index.js
@@ -41,7 +41,7 @@ var VariantCurationHub = React.createClass({
             this.setState({variantObj: response});
             this.setState({isLoadingComplete: true});
         }).catch(function(e) {
-            console.log('GETGDM ERROR=: %o', e);
+            console.log('FETCH CLINVAR ERROR=: %o', e);
         });
     },
 
@@ -60,7 +60,7 @@ var VariantCurationHub = React.createClass({
         return (
             <div>
                 <VariantCurationHeader variantData={variantData} interpretationUuid={interpretationUuid} session={session} />
-                <VariantCurationActions variantData={variantData} interpretation={interpretation} eidtKey={editKey} session={session} />
+                <VariantCurationActions variantData={variantData} interpretation={interpretation} editKey={editKey} session={session} />
                 <VariantCurationInterpretation variantData={variantData} interpretation={interpretation} editKey={editKey} session={session}
                     href_url={this.props.href_url} updateInterpretationObj={this.updateInterpretationObj} />
             </div>
