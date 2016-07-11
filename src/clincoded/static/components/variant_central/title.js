8ee79a8573ff649422004dc45b6131a2b0f5704e
Jimmy Zhen <jzhen@stanford.edu>
2016-07-11 10:29:09 -0700

    Updated code to display associated disease in subtitle

--- src/clincoded/static/components/variant_central/title.js
+++ src/clincoded/static/components/variant_central/title.js
@@ -4,14 +4,22 @@ var React = require('react');
 // General purpose title rendering
 var Title = module.exports.Title = React.createClass({
     propTypes: {
-        data: React.PropTypes.object // ClinVar data payload
+        data: React.PropTypes.object, // ClinVar data payload
+        interpretation: React.PropTypes.object
     },
 
     render: function() {
         var variant = this.props.data;
+        var disease_term = '';
+        if (this.props.interpretation) {
+            if (this.props.interpretation.disease) {
+                disease_term = this.props.interpretation.disease.term;
+            }
+        }
+
         if (variant) {
             var clinVarTitle = (variant.clinvarVariantTitle) ? variant.clinvarVariantTitle : 'A preferred title is not available';
-            var associatedDisease = (variant.disease) ? variant.disease : 'Not yet associated with a disease';
+            var associatedDisease = (disease_term) ? 'This interpretation is associated with disease: ' + disease_term : 'Not yet associated with a disease';
         }
 
         return (
