49ae021005087bbb8c8c4e6bedbc25412eda86df
Minyoung Choi <minchoi@stanford.edu>
2016-07-11 13:07:42 -0700

    begin implementing CI

--- src/clincoded/static/components/variant_central/interpretation/population.js
+++ src/clincoded/static/components/variant_central/interpretation/population.js
@@ -38,7 +38,7 @@ var populationStatic = {
 
 // Display the population data of external sources
 var CurationInterpretationPopulation = module.exports.CurationInterpretationPopulation = React.createClass({
-    mixins: [RestMixin],
+    mixins: [RestMixin, FormMixin],
 
     propTypes: {
         data: React.PropTypes.object, // ClinVar data payload
@@ -454,6 +454,61 @@ var CurationInterpretationPopulation = module.exports.CurationInterpretationPopu
         );
     },
 
+    changeDesiredCI: function(ref, e) {
+        console.log(ref);
+        console.log(this.refs);
+        //this.calculateCI(CIp, highestMAF);
+    },
+
+    // function to calculate confidence intervals (CI). Formula taken from Steven's excel spreadsheet
+    calculateCI: function(CIp, highestMAF) {
+        //let CIp = this.refs[ref].getValue();
+        if (highestMAF && 0 <= CIp <= 100) {
+            let xp = highestMAF.ac,
+                np = highestMAF.ac_tot,
+                CIp = 95;
+            let zp = -this.normSInv((1 - CIp / 100) / 2),
+                pp = xp / np,
+                qp = 1 - pp;
+            let CILow = ((2 * np * pp) + (zp * zp) - zp * Math.sqrt((zp * zp) + (4 * np * pp * qp))) / (2 * (np + (zp * zp))),
+                CIHigh = ((2 * np * pp) + (zp * zp) + zp * Math.sqrt((zp * zp) + (4 * np * pp * qp))) / (2 * (np + (zp * zp)));
+            console.log(zp);
+            console.log(CILow);
+            console.log(CIHigh);
+        }
+    },
+
+    // NORMSINV implementation taken from http://stackoverflow.com/a/8843728
+    // used for CI calculation
+    normSInv: function (p) {
+        let a1 = -39.6968302866538, a2 = 220.946098424521, a3 = -275.928510446969;
+        let a4 = 138.357751867269, a5 = -30.6647980661472, a6 = 2.50662827745924;
+        let b1 = -54.4760987982241, b2 = 161.585836858041, b3 = -155.698979859887;
+        let b4 = 66.8013118877197, b5 = -13.2806815528857, c1 = -7.78489400243029E-03;
+        let c2 = -0.322396458041136, c3 = -2.40075827716184, c4 = -2.54973253934373;
+        let c5 = 4.37466414146497, c6 = 2.93816398269878, d1 = 7.78469570904146E-03;
+        let d2 = 0.32246712907004, d3 = 2.445134137143, d4 = 3.75440866190742;
+        let p_low = 0.02425, p_high = 1 - p_low;
+        let q, r;
+        let retVal;
+
+        if ((p < 0) || (p > 1)) {
+            alert("NormSInv: Argument out of range.");
+            retVal = 0;
+        } else if (p < p_low) {
+            q = Math.sqrt(-2 * Math.log(p));
+            retVal = (((((c1 * q + c2) * q + c3) * q + c4) * q + c5) * q + c6) / ((((d1 * q + d2) * q + d3) * q + d4) * q + 1);
+        } else if (p <= p_high) {
+            q = p - 0.5;
+            r = q * q;
+            retVal = (((((a1 * r + a2) * r + a3) * r + a4) * r + a5) * r + a6) * q / (((((b1 * r + b2) * r + b3) * r + b4) * r + b5) * r + 1);
+        } else {
+            q = Math.sqrt(-2 * Math.log(1 - p));
+            retVal = -(((((c1 * q + c2) * q + c3) * q + c4) * q + c5) * q + c6) / ((((d1 * q + d2) * q + d3) * q + d4) * q + 1);
+        }
+        return retVal;
+    },
+
     render: function() {
         var exacStatic = populationStatic.exac,
             tGenomesStatic = populationStatic.tGenomes,
@@ -494,8 +549,11 @@ var CurationInterpretationPopulation = module.exports.CurationInterpretationPopu
                             <dl className="inline-dl clearfix">
                                 <dt>Source: </dt><dd>{highestMAF && highestMAF.source ? highestMAF.source : 'N/A'}</dd>
                                 <dt>Allele Frequency: </dt><dd>{highestMAF && highestMAF.af ? highestMAF.af : 'N/A'}</dd>
-                                {(this.state.interpretation) ?
+                                {(this.state.interpretation && highestMAF) ?
                                     <span>
+                                        <dt>Desired CI:</dt><dd>
+                                        <Form><input type="number" name="desiredCI" defaultValue={"95"} onChange={this.changeDesiredCI} maxLength="2" /></Form>
+                                        </dd>
                                         <dt>CI - lower: </dt><dd>XXXXXX</dd>
                                         <dt>CI - upper: </dt><dd>XXXXXX</dd>
                                     </span>
