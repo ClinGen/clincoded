3da365f27f67992805a46af49623ebafde664595
Jimmy Zhen <jzhen@stanford.edu>
2016-07-11 17:12:04 -0700

    Updated code to handle state change upon closing modal of associating disease

--- src/clincoded/static/components/variant_central/actions.js
+++ src/clincoded/static/components/variant_central/actions.js
@@ -20,25 +20,34 @@ var queryKeyValue = globals.queryKeyValue;
 var VariantCurationActions = module.exports.VariantCurationActions = React.createClass({
     mixins: [RestMixin, ModalMixin, FormMixin, CuratorHistory],
 
+    childContextTypes: {
+        handleStateChange: React.PropTypes.func
+    },
+
+    getChildContext: function() {
+        return {handleStateChange: this.handleStateChange};
+    },
+
     propTypes: {
         variantData: React.PropTypes.object, // ClinVar data payload
         session: React.PropTypes.object,
         interpretation: React.PropTypes.object,
         editKey: React.PropTypes.string,
-        href_url: React.PropTypes.string
+        href_url: React.PropTypes.string,
+        updateInterpretationObj: React.PropTypes.func
     },
 
     getInitialState: function() {
         return {
             variantUuid: null,
             hasExistingInterpretation: false,
-            isEditMode: false,
+            isInterpretationActive: false,
             hasAssociatedDisease: false
         };
     },
 
     componentWillReceiveProps: function(nextProps) {
-        if (nextProps.variantData && this.props.variantData) {
+        if (this.props.variantData) {
             if (this.props.variantData.associatedInterpretations) {
                 if (this.props.variantData.associatedInterpretations.length && this.props.variantData.submitted_by['@id'] === this.props.session.user_properties['@id']) {
                     this.setState({hasExistingInterpretation: true});
@@ -46,18 +55,22 @@ var VariantCurationActions = module.exports.VariantCurationActions = React.creat
             }
         }
         if (this.props.editKey === 'true' && this.props.interpretation) {
-            this.setState({isEditMode: true});
+            this.setState({isInterpretationActive: true});
             if (this.props.interpretation.interpretation_disease) {
                 this.setState({hasAssociatedDisease: true});
             }
         }
     },
 
+    handleStateChange: function() {
+        this.setState({hasAssociatedDisease: true});
+    },
+
     // handler for 'Start new interpretation' & 'Continue interpretation' button click events
     handleInterpretationEvent: function(e) {
         e.preventDefault(); e.stopPropagation();
         var variantObj = this.props.variantData;
-        var newInterpretationObj;
+        var newInterpretationObj, href_url;
         if (!this.state.hasExistingInterpretation) {
             if (variantObj) {
                 this.setState({variantUuid: variantObj.uuid});
@@ -70,7 +83,7 @@ var VariantCurationActions = module.exports.VariantCurationActions = React.creat
                 var newInterpretationUuid = data['@graph'][0].uuid;
                 window.location.href = '/variant-central/?edit=true&variant=' + this.state.variantUuid + '&interpretation=' + newInterpretationUuid;
             }).catch(e => {parseAndLogError.bind(undefined, 'postRequest')});
-        } else if (this.state.hasExistingInterpretation && !this.isEditMode) {
+        } else if (this.state.hasExistingInterpretation && !this.state.isInterpretationActive) {
             window.location.href = '/variant-central/?edit=true&variant=' + variantObj.uuid + '&interpretation=' + variantObj.associatedInterpretations[0].uuid;
         }
     },
@@ -79,7 +92,7 @@ var VariantCurationActions = module.exports.VariantCurationActions = React.creat
         var interpretationButtonTitle = '';
         if (!this.state.hasExistingInterpretation) {
             interpretationButtonTitle = 'Start New Interpretation';
-        } else if (this.state.hasExistingInterpretation && !this.isEditMode) {
+        } else if (this.state.hasExistingInterpretation && !this.state.isInterpretationActive) {
             interpretationButtonTitle = 'Continue Interpretation';
         }
 
@@ -95,14 +108,14 @@ var VariantCurationActions = module.exports.VariantCurationActions = React.creat
         return (
             <Form formClassName="form-horizontal form-std">
                 <div className="container curation-actions curation-variant">
-                    {(this.state.isEditMode) ?
+                    {(this.state.isInterpretationActive) ?
                         <div className="interpretation-record clearfix">
                             <h2><span>Variant Interpretation Record</span></h2>
                             <div className="btn-group">
                                 <Modal title={associateDiseaseModalTitle} wrapperClassName="modal-associate-disease">
                                     <button className="btn btn-primary pull-right"
                                         modal={<AssociateDisease closeModal={this.closeModal} data={this.props.variantData} url={this.props.href_url} session={this.props.session}
-                                            interpretation={this.props.interpretation} editKey={this.props.editkey} />}>{associateDiseaseButtonTitle}</button>
+                                            interpretation={this.props.interpretation} editKey={this.props.editkey} updateInterpretationObj={this.props.updateInterpretationObj} />}>{associateDiseaseButtonTitle}</button>
                                 </Modal>
                             </div>
                         </div>
@@ -121,13 +134,18 @@ var VariantCurationActions = module.exports.VariantCurationActions = React.creat
 var AssociateDisease = React.createClass({
     mixins: [RestMixin, FormMixin],
 
+    contextTypes: {
+        handleStateChange: React.PropTypes.func
+    },
+
     propTypes: {
         data: React.PropTypes.object,
         session: React.PropTypes.object,
         closeModal: React.PropTypes.func, // Function to call to close the modal
         interpretation: React.PropTypes.object,
         editKey: React.PropTypes.bool,
-        url: React.PropTypes.string
+        url: React.PropTypes.string,
+        updateInterpretationObj: React.PropTypes.func
     },
 
     getInitialState: function() {
@@ -178,6 +196,8 @@ var AssociateDisease = React.createClass({
                         flatInterpretation.disease = '';
                         // Return the newly flattened interpretation object in a Promise
                         return Promise.resolve(flatInterpretation);
+                    } else {
+                        return Promise.resolve(flatInterpretation);
                     }
                 }).then(interpretationObj => {
                     if (interpretationDisease) {
@@ -185,9 +205,9 @@ var AssociateDisease = React.createClass({
                         interpretationObj.disease = interpretationDisease;
                         // Update the intepretation object partially with the new disease property value
                         return this.putRestData('/interpretation/' + this.props.interpretation.uuid, interpretationObj).then(result => {
-                            return Promise.resolve(result['@graph'][0]);
+                            this.props.updateInterpretationObj(result);
                             this.props.closeModal();
-                            window.location.replace(this.props.url);
+                            window.history.replaceState(window.state, '', this.props.url);
                         });
                     }
                 });
@@ -201,16 +221,21 @@ var AssociateDisease = React.createClass({
     // Called when the modal 'Cancel' button is clicked
     cancelAction: function(e) {
         this.props.closeModal();
-        window.location.replace(this.props.url);
+        window.history.replaceState(window.state, '', this.props.url);
     },
 
     render: function() {
-        var disease = this.props.interpretation.interpretation_disease;
+        var disease_id = '';
+        if (this.props.interpretation) {
+            if (this.props.interpretation.interpretation_disease) {
+                disease_id = this.props.interpretation.interpretation_disease;
+            }
+        }
 
         return (
             <div className="modal-box">
                 <div className="modal-body">
-                    <Input type="text" ref="orphanetid" label={<LabelOrphanetId />} placeholder="e.g. ORPHA15" defaultValue={(disease) ? disease : null}
+                    <Input type="text" ref="orphanetid" label={<LabelOrphanetId />} placeholder="e.g. ORPHA15" value={(disease_id) ? disease_id : null}
                         error={this.getFormError('orphanetid')} clearError={this.clrFormErrors.bind(null, 'orphanetid')}
                         labelClassName="col-sm-4 control-label" wrapperClassName="col-sm-7" groupClassName="form-group" inputClassName="uppercase-input" required />
                 </div>
