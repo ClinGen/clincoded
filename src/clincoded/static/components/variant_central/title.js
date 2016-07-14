5801e37d50d0bb977f25e57ad8b1e68f052747e9
Jimmy Zhen <jzhen@stanford.edu>
2016-07-13 18:25:52 -0700

    Updated code to handle disease term change in subtitle

--- src/clincoded/static/components/variant_central/title.js
+++ src/clincoded/static/components/variant_central/title.js
@@ -1,5 +1,6 @@
 'use strict';
 var React = require('react');
+var _ = require('underscore');
 
 // General purpose title rendering
 var Title = module.exports.Title = React.createClass({
@@ -8,9 +9,44 @@ var Title = module.exports.Title = React.createClass({
         interpretation: React.PropTypes.object
     },
 
+    getInitialState: function() {
+        return {
+            interpretation: null // parent interpretation object
+        };
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        // this block is for handling props and states when props (external data) is updated after the initial load/rendering
+        // when props are updated, update the parent interpreatation object, if applicable
+        if (typeof nextProps.interpretation !== undefined && !_.isEqual(nextProps.interpretation, this.props.interpretation)) {
+            this.setState({interpretation: nextProps.interpretation});
+        }
+    },
+
+    renderSubtitle: function(interpretation, variant) {
+        var associatedDisease = 'Not yet associated with a disease';
+        if (interpretation) {
+            if (interpretation.disease) {
+                if (interpretation.disease.term) {
+                    associatedDisease = 'This interpretation is associated with disease: ' + interpretation.disease.term;
+                }
+            }
+        }
+        if (variant && !interpretation) {
+            if (variant.associatedInterpretations.length) {
+                if (variant.associatedInterpretations[0].disease) {
+                    if (variant.associatedInterpretations[0].disease.term) {
+                        associatedDisease = 'This interpretation is associated with disease: ' + variant.associatedInterpretations[0].disease.term;
+                    }
+                }
+            }
+        }
+        return associatedDisease;
+    },
+
     render: function() {
         var variant = this.props.data;
-        var associatedDisease = '';
+        var interpretation = this.state.interpretation;
 
         var variantTitle = (variant && variant.clinvarVariantTitle) ? variant.clinvarVariantTitle : null;
         if (variant && !variantTitle && variant.hgvsNames && variant.hgvsNames != {}) {
@@ -19,18 +55,10 @@ var Title = module.exports.Title = React.createClass({
             variantTitle = 'A preferred title is not available';
         }
 
-        if (variant) {
-            if (variant.associatedInterpretations.length) {
-                if (variant.associatedInterpretations[0].disease) {
-                    associatedDisease = (disease_term) ? 'This interpretation is associated with disease: ' + variant.associatedInterpretations[0].disease.term : 'Not yet associated with a disease';
-                }
-            }
-        }
-
         return (
             <div>
                 <h1>{variantTitle}{this.props.children}</h1>
-                <h2>{associatedDisease}</h2>
+                <h2>{this.renderSubtitle(interpretation, variant)}</h2>
             </div>
         );
     }
