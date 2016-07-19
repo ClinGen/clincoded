06045d9694bafec0e9b0473dfe6667993fba57c7
Minyoung Choi <minchoi@stanford.edu>
2016-07-19 16:26:38 -0700

    re-add missing line

--- src/clincoded/static/components/variant_central/interpretation.js
+++ src/clincoded/static/components/variant_central/interpretation.js
@@ -111,6 +111,7 @@ var VariantCurationInterpretation = module.exports.VariantCurationInterpretation
                     </div>
                     <div role="tabpanel" className={"tab-panel" + (this.state.selectedTab == 'population' ? '' : ' hidden')}>
                         <CurationInterpretationPopulation data={variant} protocol={this.props.href_url.protocol}
+                            interpretation={interpretation} updateInterpretationObj={this.props.updateInterpretationObj}
                             ext_myVariantInfo={this.state.ext_myVariantInfo}
                             ext_ensemblVEP={this.state.ext_ensemblVEP}
                             ext_ensemblVariation={this.state.ext_ensemblVariation} />
