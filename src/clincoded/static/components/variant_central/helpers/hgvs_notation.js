f2c7310119d52b28ba51f8be01a77d855ed36da6
Jimmy Zhen <jzhen@stanford.edu>
2016-07-25 14:19:40 -0700

    Updated code to implement descriptive variable names and inline comments

--- src/clincoded/static/components/variant_central/helpers/hgvs_notation.js
+++ src/clincoded/static/components/variant_central/helpers/hgvs_notation.js
@@ -1,49 +1,44 @@
 // # HGVS Notation Helper: Getter Method
 // # Parameters: variant object, assembly (GRCh37 or GRCh38), optional boolean value
 // # Usage: getHgvsNotation(variant, 'GRCh38', true)
-//
-// This method extracts the genomic substring from HGVS name whose assembly is
-// either GRCh37 or GRCh38. By looking up the genomic-to-chromosome mappings,
-// it formats the HGVS notation either as 'chr3:g.70970756G>A' or '3:g.70970756G>A',
-// depending on whether the 'chr' optional argument is set to true.
-//
-// The 'chr3:g.70970756G>A' format is for myvariant.info that uses GRCh37 assembly.
-// The '3:g.70970756G>A' format is for Ensembl VEP that uses GRCh38 assembly. Due to the
-// presence of 'chrX' and 'chrY' chromosome, substr method is used instead of filtering
-// alpha letters.
 
 'use strict';
-var genomic_chr_mapping = require('./interpretation/mapping/NC_genomic_chr_format.json');
+var genomic_chr_mapping = require('../interpretation/mapping/NC_genomic_chr_format.json');
 
-const getHgvsNotation = function (variant, assembly, omitChrString) {
-    let hgvs;
-    let nc;
-    let ncGenomic;
-    let match;
+export function getHgvsNotation(variant, assembly, omitChrString) {
+    let hgvs,
+        genomicHGVS;
 
     if (assembly === 'GRCh37' && variant.hgvsNames.GRCh37) {
-        nc = variant.hgvsNames.GRCh37;
+        genomicHGVS = variant.hgvsNames.GRCh37;
     } else if (assembly === 'GRCh38' && variant.hgvsNames.GRCh38) {
-        nc = variant.hgvsNames.GRCh38;
+        genomicHGVS = variant.hgvsNames.GRCh38;
     }
 
-    ncGenomic = nc.substr(0, nc.indexOf(':'));
-    match = genomic_chr_mapping[assembly].find((entry) => entry.GenomicRefSeq === ncGenomic);
+    // Extract the NC genomic substring from the HGVS name whose assembly is
+    // either GRCh37 or GRCh38. By looking up the genomic-to-chromosome mappings,
+    // it formats the HGVS notation either as 'chr3:g.70970756G>A' or '3:g.70970756G>A',
+    // depending on whether the 'omitChrString' optional argument is set to true.
+    let ncGenomic = genomicHGVS.substr(0, genomicHGVS.indexOf(':'));
+    let match = genomic_chr_mapping[assembly].find((entry) => entry.GenomicRefSeq === ncGenomic);
 
+    // The 'chr3:g.70970756G>A' format is for myvariant.info that uses GRCh37 assembly.
+    // The '3:g.70970756G>A' format is for Ensembl VEP that uses GRCh38 assembly. Due to the
+    // presence of 'chrX' and 'chrY' chromosome, substr method is used instead of filtering
+    // alpha letters.
     if (match) {
         if (omitChrString) {
             let chromosome = match.ChrFormat.substr(3);
-            hgvs = chromosome + nc.slice(nc.indexOf(':'));
+            hgvs = chromosome + genomicHGVS.slice(genomicHGVS.indexOf(':'));
         } else {
-            hgvs = match.ChrFormat + nc.slice(nc.indexOf(':'));
+            hgvs = match.ChrFormat + genomicHGVS.slice(genomicHGVS.indexOf(':'));
         }
     }
 
+    // Handle deleted genomic HGVS
     if (hgvs && hgvs.indexOf('del') > 0) {
         hgvs = hgvs.substring(0, hgvs.indexOf('del') + 3);
     }
 
     return hgvs;
-};
-
-module.exports = getHgvsNotation;
+}
