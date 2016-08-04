f630c663ffb30683c850e25372870ad43319eea6
Minyoung Choi <minchoi@stanford.edu>
2016-08-04 12:52:51 -0700

    add forms to functional tab

--- src/clincoded/static/components/variant_central/interpretation/functional.js
+++ src/clincoded/static/components/variant_central/interpretation/functional.js
@@ -44,19 +44,80 @@ var CurationInterpretationFunctional = module.exports.CurationInterpretationFunc
             <div className="variant-interpretation functional">
                 <PanelGroup accordion><Panel title="Does variant result in LOF?" panelBodyClassName="panel-wide-content" open>
                     {(this.props.data && this.state.interpretation) ?
-                        <div className="panel panel-info">
-                            <div className="panel-body">
-                                <CurationInterpretationForm renderedFormContent={criteriaLof1} criteria={['PVS1']}
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <CurationInterpretationForm renderedFormContent={criteriaGroup1} criteria={['PM1']}
                                     evidenceData={null} evidenceDataUpdated={true}
-                                    formDataUpdater={criteriaLof1Update} variantUuid={this.props.data['@id']}
+                                    formDataUpdater={criteriaGroup1Update} variantUuid={this.props.data['@id']}
                                     interpretation={this.state.interpretation} updateInterpretationObj={this.props.updateInterpretationObj} />
                             </div>
                         </div>
                     : null}
                 </Panel></PanelGroup>
                 <PanelGroup accordion><Panel title="Is LOF known mechanism for disease of interest?" panelBodyClassName="panel-wide-content" open>
+                    {(this.props.data && this.state.interpretation) ?
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <CurationInterpretationForm renderedFormContent={criteriaGroup2} criteria={['BS3', 'PS3']}
+                                    evidenceData={null} evidenceDataUpdated={true} criteriaCrossCheck={[['BS3', 'PS3']]}
+                                    formDataUpdater={criteriaGroup2Update} variantUuid={this.props.data['@id']} formChangeHandler={criteriaGroup2Change}
+                                    interpretation={this.state.interpretation} updateInterpretationObj={this.props.updateInterpretationObj} />
+                            </div>
+                        </div>
+                    : null}
                 </Panel></PanelGroup>
             </div>
         );
     }
 });
+
+// code for rendering of population tab interpretation forms
+var criteriaGroup1 = function() {
+    let criteriaList1 = ['PM1'], // array of criteria code handled in this section
+        hiddenList1 = [false]; // array indicating hidden status of explanation boxes for above list of criteria codes
+    return (
+        <div>
+            {vciFormHelper.evalFormSectionWrapper.call(this,
+                vciFormHelper.evalFormNoteSectionWrapper.call(this, criteriaList1),
+                vciFormHelper.evalFormDropdownSectionWrapper.call(this, criteriaList1),
+                vciFormHelper.evalFormExplanationSectionWrapper.call(this, criteriaList1, hiddenList1, null, null),
+                false
+            )}
+        </div>
+    );
+};
+
+// code for updating the form values of population tab interpretation forms upon receiving
+// existing interpretations and evaluations
+var criteriaGroup1Update = function(nextProps) {
+    vciFormHelper.updateEvalForm.call(this, nextProps, ['PM1'], null);
+};
+
+
+// code for rendering of population tab interpretation forms
+var criteriaGroup2 = function() {
+    let criteriaList1 = ['BS3', 'PS3'], // array of criteria code handled in this section
+        hiddenList1 = [false, true]; // array indicating hidden status of explanation boxes for above list of criteria codes
+    return (
+        <div>
+            {vciFormHelper.evalFormSectionWrapper.call(this,
+                vciFormHelper.evalFormNoteSectionWrapper.call(this, criteriaList1),
+                vciFormHelper.evalFormDropdownSectionWrapper.call(this, criteriaList1),
+                vciFormHelper.evalFormExplanationSectionWrapper.call(this, criteriaList1, hiddenList1, null, null),
+                false
+            )}
+        </div>
+    );
+};
+
+// code for updating the form values of population tab interpretation forms upon receiving
+// existing interpretations and evaluations
+var criteriaGroup2Update = function(nextProps) {
+    vciFormHelper.updateEvalForm.call(this, nextProps, ['BS3', 'PS3'], null);
+};
+
+// code for handling logic within the form
+var criteriaGroup2Change = function(ref, e) {
+    // Both explanation boxes for both criteria of each group must be the same
+    vciFormHelper.shareExplanation.call(this, ref, ['BS3', 'PS3']);
+};
