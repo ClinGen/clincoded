26640d9df6d877c361fd8edbae8418d36bd5d32b
Kang Liu <kgliu0101@gmail.com>
2016-08-15 16:22:13 -0700

    remove stand alone from BA1

--- src/clincoded/static/components/variant_central/interpretation/shared/form.js
+++ src/clincoded/static/components/variant_central/interpretation/shared/form.js
@@ -451,7 +451,7 @@ function evalFormValueDropdown(criteria) {
             {criteria[1] === 'P' ? null : <option value="supporting">{criteria}_P</option>}
             {criteria[0] === 'P' && criteria[1] !== 'M' ? <option value="moderate">{criteria}_M</option> : null}
             {criteria[1] === 'S' ? null : <option value="strong">{criteria}_S</option>}
-            {criteria[0] === 'B' ? <option value="stand-alone">{criteria}_stand alone</option> : null}
+            {(criteria[0] === 'B' && criteria[1] !== 'A') ? <option value="stand-alone">{criteria}_stand alone</option> : null}
             {criteria[0] === 'P' && criteria[1] !== 'V' ? <option value="very-strong">{criteria}_VS</option> : null}
         </Input>
     );
