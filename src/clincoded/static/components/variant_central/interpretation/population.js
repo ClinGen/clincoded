139440b0239e53dd7d196c6df694cdda13258d49
Jimmy Zhen <jzhen@stanford.edu>
2016-09-01 09:28:13 -0700

    Replaced referenced to 'ext_ensemblVEP' with 'ext_ensemblHgvsVEP' in state/props

--- src/clincoded/static/components/variant_central/interpretation/population.js
+++ src/clincoded/static/components/variant_central/interpretation/population.js
@@ -53,7 +53,7 @@ var CurationInterpretationPopulation = module.exports.CurationInterpretationPopu
         interpretation: React.PropTypes.object,
         updateInterpretationObj: React.PropTypes.func,
         ext_myVariantInfo: React.PropTypes.object,
-        ext_ensemblVEP: React.PropTypes.array,
+        ext_ensemblHgvsVEP: React.PropTypes.array,
         ext_ensemblVariation: React.PropTypes.object
     },
 
@@ -111,9 +111,9 @@ var CurationInterpretationPopulation = module.exports.CurationInterpretationPopu
             this.parseEspData(this.props.ext_myVariantInfo);
             this.calculateHighestMAF();
         }
-        if (this.props.ext_ensemblVEP) {
-            this.parseAlleleFrequencyData(this.props.ext_ensemblVEP);
-            this.parseGeneConstraintScores(this.props.ext_ensemblVEP);
+        if (this.props.ext_ensemblHgvsVEP) {
+            this.parseAlleleFrequencyData(this.props.ext_ensemblHgvsVEP);
+            this.parseGeneConstraintScores(this.props.ext_ensemblHgvsVEP);
             this.calculateHighestMAF();
         }
         if (this.props.ext_ensemblVariation) {
@@ -140,9 +140,9 @@ var CurationInterpretationPopulation = module.exports.CurationInterpretationPopu
             this.parseEspData(nextProps.ext_myVariantInfo);
             this.calculateHighestMAF();
         }
-        if (nextProps.ext_ensemblVEP) {
-            this.parseAlleleFrequencyData(nextProps.ext_ensemblVEP);
-            this.parseGeneConstraintScores(nextProps.ext_ensemblVEP);
+        if (nextProps.ext_ensemblHgvsVEP) {
+            this.parseAlleleFrequencyData(nextProps.ext_ensemblHgvsVEP);
+            this.parseGeneConstraintScores(nextProps.ext_ensemblHgvsVEP);
             this.calculateHighestMAF();
         }
         if (nextProps.ext_ensemblVariation) {
