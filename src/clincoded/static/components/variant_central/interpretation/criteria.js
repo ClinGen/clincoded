f8310d15ef31aa4021d858440db7b026403cf59d
Jimmy Zhen <jzhen@stanford.edu>
2016-09-06 16:09:35 -0700

    Updated code to handle the display of 'met' and 'not-met' criteria

--- src/clincoded/static/components/variant_central/interpretation/criteria.js
+++ src/clincoded/static/components/variant_central/interpretation/criteria.js
@@ -1,20 +1,18 @@
 'use strict';
 var React = require('react');
-var globals = require('../../globals');
 var evidenceCodes = require('./mapping/evidence_code.json');
-
-var queryKeyValue = globals.queryKeyValue;
-var editQueryValue = globals.editQueryValue;
 
 // Display met criteria
 var CurationInterpretationCriteria = module.exports.CurationInterpretationCriteria = React.createClass({
     propTypes: {
-        interpretation: React.PropTypes.object
+        interpretation: React.PropTypes.object,
+        selectedTab: React.PropTypes.string
     },
 
     getInitialState: function() {
         return {
-            interpretation: this.props.interpretation
+            interpretation: this.props.interpretation,
+            selectedTab: this.props.selectedTab
         };
     },
 
@@ -22,46 +20,70 @@ var CurationInterpretationCriteria = module.exports.CurationInterpretationCriter
         if (nextProps.interpretation && this.props.interpretation) {
             this.setState({interpretation: nextProps.interpretation});
         }
+        if (nextProps.selectedTab) {
+            this.setState({selectedTab: nextProps.selectedTab});
+        }
     },
 
     // Method to render individual criteria codes and their respective tooltip
-    // 'data-status' attribute flags whether an interpretation is associated with disease
-    // 'data-evaluation' attribute flags whether a criterion is met
+    // 'data-status' attribute flags whether a criterion is met
     renderCriteriaBar: function(key, evidence, interpretation) {
-        // Flag disease-dependent criteria via @class
-        let isDiseaseDependent = (evidence[key]['disease-dependent']) ? 'disease-dependent' : 'not-disease-dependent';
-        let status = 'not-disease-associated',
-            evaluation = 'not-evaluated';
-        // Flag disease-associated interpretation via [data-status]
-        if (interpretation.interpretation_disease) {
-            status = 'disease-associated';
-        }
-        // Flag 'met' criteria via [data-evaluation]
+        let status = 'not-evaluated';
+        // Flag 'met' criteria via [data-status]
         let evalArray = interpretation.evaluations;
         if (evalArray) {
             if (evalArray.length) {
                 evalArray.forEach(entry => {
                     if (typeof entry.criteria !== 'undefined' && entry.criteria === key) {
-                        evaluation = entry.value;
+                        status = entry.criteriaStatus;
                     }
                 });
             }
         }
         return (
-            <button className={'btn btn-default ' + evidence[key].class + ' ' + evidence[key].category + ' ' + isDiseaseDependent}
-                type="button" key={key} data-status={status} data-evaluation={evaluation}
+            <button className={'btn btn-default ' + evidence[key].class + ' ' + evidence[key].category
+                + this.getCurrentTab(evidence[key].category)}
+                type="button" key={key} data-status={status} data-
                 data-toggle="tooltip" data-placement="top" data-tooltip={evidence[key].definition}>
                 <span>{key}</span>
             </button>
         );
     },
 
-    render: function() {
+    // Method to return current tab
+    getCurrentTab: function(category) {
+        let currentTabName, className = '';
+        let currentTab = this.state.selectedTab;
+        if (currentTab) {
+            switch (currentTab) {
+                case 'population':
+                    currentTabName = 'population';
+                    break;
+                case 'predictors':
+                    currentTabName = 'computational';
+                    break;
+                case 'experimental':
+                    currentTabName = 'functional';
+                    break;
+                case 'segregation-case':
+                    currentTabName = 'segregation';
+                    break;
+                default:
+                    currentTabName = null;
+            }
+        }
+        if (currentTabName && currentTabName === category) {
+            className = ' onCurrentTab';
+        }
+        return className;
+    },
+
+    render() {
         var keys = Object.keys(evidenceCodes);
         var interpretation = this.state.interpretation;
 
         return (
-            <div className="curation-criteria curation-variant">
+            <div className="container curation-criteria curation-variant">
                 {(interpretation) ?
                     <div className="criteria-bar btn-toolbar" role="toolbar" aria-label="Criteria bar with code buttons">
                         <div className="criteria-group btn-group btn-group-sm" role="group" aria-label="Criteria code button group">
