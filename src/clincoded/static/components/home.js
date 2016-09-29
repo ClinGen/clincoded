17be57206f2e3d56c2da6fab2331460ae23ce04a
Jimmy Zhen <jzhen@stanford.edu>
2016-09-29 15:15:55 -0700

    Added border to the bottom 2 panels

--- src/clincoded/static/components/home.js
+++ src/clincoded/static/components/home.js
@@ -37,23 +37,27 @@ var Home = module.exports.Home = React.createClass({
                     </div>
                     <div className="row">
                         <div className="col-sm-6">
-                            <h2>Variant Curation</h2>
-                            <h3>Which changes in the gene cause disease?</h3>
-                            <p>
-                                The ClinGen variant curation process combines clinical, genetic, population, and functional evidence with expert review to classify variants into 1 of 5 categories according to the <a href="https://www.acmg.net/docs/standards_guidelines_for_the_interpretation_of_sequence_variants.pdf" target="_blank">ACMG guidelines <i className="icon icon-file-pdf-o"></i></a>.
-                            </p>
-                            <p><strong>Pathogenic &#8226; Likely Pathogenic &#8226; Uncertain &#8226; Likely Benign &#8226; Benign</strong></p>
-                            <p className="help-document">
-                                <a className="btn btn-primary" href="/static/help/clingen-variant-curation-help.pdf" target="_blank" role="button">Learn more »</a>
-                            </p>
+                            <div className="promo">
+                                <h2>Variant Curation</h2>
+                                <h3>Which changes in the gene cause disease?</h3>
+                                <p>
+                                    The ClinGen variant curation process combines clinical, genetic, population, and functional evidence with expert review to classify variants into 1 of 5 categories according to the <a href="https://www.acmg.net/docs/standards_guidelines_for_the_interpretation_of_sequence_variants.pdf" target="_blank">ACMG guidelines <i className="icon icon-file-pdf-o"></i></a>.
+                                </p>
+                                <p><strong>Pathogenic &#8226; Likely Pathogenic &#8226; Uncertain &#8226; Likely Benign &#8226; Benign</strong></p>
+                                <p className="help-document">
+                                    <a className="btn btn-primary" href="/static/help/clingen-variant-curation-help.pdf" target="_blank" role="button">Learn more »</a>
+                                </p>
+                            </div>
                         </div>
                         <div className="col-sm-6">
-                            <h2>Gene Curation</h2>
-                            <h3>Does variation in this gene cause disease?</h3>
-                            <p>
-                                The ClinGen gene curation process combines an appraisal of genetic and experimental data in the scientific literature with expert review to classify gene-disease pairs into 1 of 6 categories according to the <a href="https://www.clinicalgenome.org/site/assets/files/2657/current_clinical_validity_classifications.pdf" target="_blank">Gene-Disease Clinical Validity Classification <i className="icon icon-file-pdf-o"></i></a> framework.
-                            </p>
-                            <p><strong>Definitive &#8226; Strong &#8226; Moderate &#8226; Limited &#8226; Disputed &#8226; Refuted</strong></p>
+                            <div className="promo">
+                                <h2>Gene Curation</h2>
+                                <h3>Does variation in this gene cause disease?</h3>
+                                <p>
+                                    The ClinGen gene curation process combines an appraisal of genetic and experimental data in the scientific literature with expert review to classify gene-disease pairs into 1 of 6 categories according to ClinGen's <a href="https://www.clinicalgenome.org/site/assets/files/2657/current_clinical_validity_classifications.pdf" target="_blank">Gene-Disease Clinical Validity Classification <i className="icon icon-file-pdf-o"></i></a> framework.
+                                </p>
+                                <p><strong>Definitive &#8226; Strong &#8226; Moderate &#8226; Limited &#8226; Disputed &#8226; Refuted</strong></p>
+                            </div>
                         </div>
                     </div>
                     <hr/>
