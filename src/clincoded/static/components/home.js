3ecff3b1320d3a381fbea32bc7fc84a456c69c67
Jimmy Zhen <jzhen@stanford.edu>
2016-09-29 11:51:10 -0700

    Added new ClinGen description content on landing page

--- src/clincoded/static/components/home.js
+++ src/clincoded/static/components/home.js
@@ -28,12 +28,38 @@ var Home = module.exports.Home = React.createClass({
                     <div className="row">
                         <div className="col-sm-12">
                             <div className="project-info site-title">
-                                <h1>ClinGen Curator Interface</h1>
-                                <p>Access to this interface is currently restricted to ClinGen curators. To access publicly available information, please visit the <a href="http://clinicalgenome.org">ClinGen portal</a>.</p>
-                                <p>If you are a ClinGen curator, you may <a href='mailto:clingen-helpdesk@lists.stanford.edu'>request an account</a>.</p>
+                                <h1>ClinGen Curator Interfaces</h1>
+                                <h4>Variant Curation &#8226; Gene Curation</h4>
+                                <p className="lead">Access to these interfaces is currently restricted to ClinGen curators. If you are a ClinGen curator, you may request an account at <a href="mailto:clingen-helpdesk@lists.stanford.edu">clingen-helpdesk@lists.stanford.edu <i className="icon icon-envelope"></i></a>.</p>
+                                <p className="lead">ClinGen is a National Institutes of Health (NIH)-funded resource dedicated to building an authoritative central resource that defines the clinical relevance of genes and variants for use in precision medicine and research. One of the key goals of ClinGen is to implement an evidence-based consensus for curating genes and variants. For more information on the ClinGen resource, please visit the ClinGen portal at <a href="https://www.clinicalgenome.org" target="_blank">clinicalgenome.org <i className="icon icon-external-link"></i></a>.</p>
                             </div>
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <h2>Variant Curation</h2>
+                            <h3>Which changes in the gene cause disease?</h3>
+                            <p>
+                                The ClinGen variant curation process combines clinical, genetic, population, and functional evidence with expert review to classify variants into 1 of 5 categories according to the <a href="https://www.acmg.net/docs/standards_guidelines_for_the_interpretation_of_sequence_variants.pdf" target="_blank">ACMG guidelines <i className="icon icon-file-pdf-o"></i></a>.
+                            </p>
+                            <p><strong>Pathogenic &#8226; Likely Pathogenic &#8226; Uncertain &#8226; Likely Benign &#8226; Benign</strong></p>
+                            <p className="help-document">
+                                <a className="btn btn-primary" href="/static/help/clingen-variant-curation-help.pdf" target="_blank" role="button">Learn more »</a>
+                            </p>
+                        </div>
+                        <div className="col-sm-6">
+                            <h2>Gene Curation</h2>
+                            <h3>Does variation in this gene cause disease?</h3>
+                            <p>
+                                The ClinGen gene curation process combines an appraisal of genetic and experimental data in the scientific literature with expert review to classify gene-disease pairs into 1 of 6 categories according to the <a href="https://www.clinicalgenome.org/site/assets/files/2657/current_clinical_validity_classifications.pdf" target="_blank">Gene-Disease Clinical Validity Classification <i className="icon icon-file-pdf-o"></i></a> framework.
+                            </p>
+                            <p><strong>Definitive &#8226; Strong &#8226; Moderate &#8226; Limited &#8226; Disputed &#8226; Refuted</strong></p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <footer>
+                        <p>&copy; 2016 <a href="https://www.clinicalgenome.org" target="_blank">ClinGen</a> - All rights reserved</p>
+                    </footer>
                 </div>
             </div>
         );
