6e2d1d20b460981b14ad1ccd2747c5544c380ae9
Jimmy Zhen <jzhen@stanford.edu>
2016-10-12 17:40:53 -0700

    Added 'case_control_curation' to load ordering

--- src/clincoded/static/components/index.js
+++ src/clincoded/static/components/index.js
@@ -27,6 +27,7 @@ require('./family_curation');
 require('./family_submit');
 require('./individual_curation');
 require('./individual_submit');
+require('./case_control_curation');
 require('./experimental_curation');
 require('./experimental_submit');
 require('./variant_curation');
