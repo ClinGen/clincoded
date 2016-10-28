65a587b90629fbf9ff68aa8201e9a392a3e6e08b
Jimmy Zhen <jzhen@stanford.edu>
2016-10-27 18:03:41 -0700

    Added case-control-submit page

--- src/clincoded/static/components/index.js
+++ src/clincoded/static/components/index.js
@@ -28,6 +28,7 @@ require('./family_submit');
 require('./individual_curation');
 require('./individual_submit');
 require('./case_control_curation');
+require('./case_control_submit');
 require('./experimental_curation');
 require('./experimental_submit');
 require('./variant_curation');
