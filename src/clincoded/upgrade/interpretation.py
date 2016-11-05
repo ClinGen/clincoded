69776a75e4b9efbeed60404152fa6db7d2ef934f
Karen Dalton <kdalton@stanford.edu>
2016-11-04 22:23:45 -0700

    update modes of inheritance for GCI/VCI

--- src/clincoded/upgrade/interpretation.py
+++ src/clincoded/upgrade/interpretation.py
@@ -1,14 +1,8 @@
 from contentbase.upgrader import upgrade_step
 
 
-@upgrade_step('gdm', '1', '2')
-def gdm_1_2(value, system):
-    # https://github.com/ClinGen/clincoded/issues/453
-    value['status'] = 'in progress'
-
-
-@upgrade_step('gdm', '2', '3')
-def gdm_2_3(value, system):
+@upgrade_step('interpretation', '1', '2')
+def interpretation_1_2(value, system):
     # https://github.com/ClinGen/clincoded/issues/1103
     if value['modeInheritance'] == 'X-linked recessive inheritance (HP:0001419)':
         value['modeInheritance'] = 'X-linked inheritance (HP:0001417)'
