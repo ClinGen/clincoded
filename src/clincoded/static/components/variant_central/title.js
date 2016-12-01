c8f16bee48462505f1d8cda6de379fcddcda8f76
Jimmy Zhen <jzhen@stanford.edu>
2016-11-30 16:18:21 -0800

    Changed subtitle display so that it always shows the core MOI, and the adjective will be shown in parentheses if an adjective exists.

--- src/clincoded/static/components/variant_central/title.js
+++ src/clincoded/static/components/variant_central/title.js
@@ -48,11 +48,11 @@ var Title = module.exports.Title = React.createClass({
         let modeInheritanceAdjective = interpretation && interpretation.modeInheritanceAdjective ? interpretation.modeInheritanceAdjective.match(/^(.*?)(?: \(HP:[0-9]*?\)){0,1}$/)[1] : null;
         if (interpretation) {
             if (interpretation.disease && interpretation.disease.term && interpretation.modeInheritance) {
-                associatedDisease = <span>This interpretation is associated with <strong>{interpretation.disease.term}</strong> - <i>{modeInheritanceAdjective ? modeInheritanceAdjective : mode}</i></span>;
+                associatedDisease = <span>This interpretation is associated with <strong>{interpretation.disease.term}</strong> - <i>{modeInheritanceAdjective ? mode + ' (' + modeInheritanceAdjective + ')' : mode}</i></span>;
             } else if (interpretation.disease && interpretation.disease.term) {
                 associatedDisease = <span>This interpretation is associated with <strong>{interpretation.disease.term}</strong></span>;
             } else if (interpretation.modeInheritance) {
-                associatedDisease = <span>This interpretation is associated with <strong>no disease</strong> - <i>{modeInheritanceAdjective ? modeInheritanceAdjective : mode}</i></span>;
+                associatedDisease = <span>This interpretation is associated with <strong>no disease</strong> - <i>{modeInheritanceAdjective ? mode + ' (' + modeInheritanceAdjective + ')' : mode}</i></span>;
             } else {
                 associatedDisease = 'This interpretation is not yet associated with a disease or mode of inheritance';
             }
