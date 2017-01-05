f52a7f64d62d885cddc0c544edadccb6d021d374
Minyoung Choi <minchoi@stanford.edu>
2017-01-05 15:17:17 -0800

    re-order dropdown ordering

--- src/clincoded/static/components/score/constants/case_info_types.js
+++ src/clincoded/static/components/score/constants/case_info_types.js
@@ -3,62 +3,62 @@
 const CASE_INFO_TYPES = {
     AUTOSOMAL_DOMINANT: [
         {
-            TYPE: 'VARIANT_IS_DE_NOVO',
-            DESCRIPTION: 'Variant is de novo'
+            TYPE: 'OTHER_VARIANT_TYPE_WITH_GENE_IMPACT',
+            DESCRIPTION: 'Proband with other variant type with some evidence of gene impact'
         },
         {
             TYPE: 'PREDICTED_OR_PROVEN_NULL_VARIANT',
             DESCRIPTION: 'Proband with predicted or proven null variant'
         },
         {
-            TYPE: 'OTHER_VARIANT_TYPE_WITH_GENE_IMPACT',
-            DESCRIPTION: 'Proband with other variant type with some evidence of gene impact'
-        }
+            TYPE: 'VARIANT_IS_DE_NOVO',
+            DESCRIPTION: 'Variant is de novo'
+        },
     ],
     X_LINKED: [
         {
-            TYPE: 'VARIANT_IS_DE_NOVO',
-            DESCRIPTION: 'Variant is de novo'
+            TYPE: 'OTHER_VARIANT_TYPE_WITH_GENE_IMPACT',
+            DESCRIPTION: 'Proband with other variant type with some evidence of gene impact'
         },
         {
             TYPE: 'PREDICTED_OR_PROVEN_NULL_VARIANT',
             DESCRIPTION: 'Proband with predicted or proven null variant'
         },
         {
-            TYPE: 'OTHER_VARIANT_TYPE_WITH_GENE_IMPACT',
-            DESCRIPTION: 'Proband with other variant type with some evidence of gene impact'
+            TYPE: 'VARIANT_IS_DE_NOVO',
+            DESCRIPTION: 'Variant is de novo'
         }
     ],
     AUTOSOMAL_RECESSIVE: [
         {
-            TYPE: 'TWO_VARIANTS_IN_TRANS_WITH_ONE_DE_NOVO',
-            DESCRIPTION: 'Two variants in trans and at least one de novo or a predicted/proven null variant'
-        },
-        {
             TYPE: 'TWO_VARIANTS_WITH_GENE_IMPACT_IN_TRANS',
             DESCRIPTION: 'Two variants (not predicted/proven null) with some evidence of gene impact in trans'
+        },
+        {
+            TYPE: 'TWO_VARIANTS_IN_TRANS_WITH_ONE_DE_NOVO',
+            DESCRIPTION: 'Two variants in trans and at least one de novo or a predicted/proven null variant'
         }
     ],
     OTHER: [
         {
-            TYPE: 'VARIANT_IS_DE_NOVO',
-            DESCRIPTION: 'Variant is de novo'
+            TYPE: 'OTHER_VARIANT_TYPE_WITH_GENE_IMPACT',
+            DESCRIPTION: 'Proband with other variant type with some evidence of gene impact'
         },
         {
             TYPE: 'PREDICTED_OR_PROVEN_NULL_VARIANT',
             DESCRIPTION: 'Proband with predicted or proven null variant'
         },
         {
-            TYPE: 'OTHER_VARIANT_TYPE_WITH_GENE_IMPACT',
-            DESCRIPTION: 'Proband with other variant type with some evidence of gene impact'
-        },
-        {
-            TYPE: 'TWO_VARIANTS_IN_TRANS_WITH_ONE_DE_NOVO',
-            DESCRIPTION: 'Two variants in trans and at least one de novo or a predicted/proven null variant'
+            TYPE: 'VARIANT_IS_DE_NOVO',
+            DESCRIPTION: 'Variant is de novo'
         },
         {
             TYPE: 'TWO_VARIANTS_WITH_GENE_IMPACT_IN_TRANS',
             DESCRIPTION: 'Two variants (not predicted/proven null) with some evidence of gene impact in trans'
+        },
+        {
+            TYPE: 'TWO_VARIANTS_IN_TRANS_WITH_ONE_DE_NOVO',
+            DESCRIPTION: 'Two variants in trans and at least one de novo or a predicted/proven null variant'
         }
     ]
 };
