d451a324a9b16673ce16d5a22bd55241fe710054
Jimmy Zhen <jzhen@stanford.edu>
2017-01-09 16:49:27 -0800

    Removed upgrade rule for alteredClassification property because there is no 'No Evidence' value found in production database; added default object properties and their values for the contradictingEvidence upgrade rule.

--- src/clincoded/upgrade/provisionalClassification.py
+++ src/clincoded/upgrade/provisionalClassification.py
@@ -4,16 +4,11 @@ from contentbase.upgrader import upgrade_step
 @upgrade_step('provisionalClassification', '1', '2')
 def provisionalClassification_1_2(value, system):
     # https://github.com/ClinGen/clincoded/issues/1196
-    if 'alteredClassification' in value:
-        if value['alteredClassification'] == 'No Evidence':
-            value['alteredClassification'] = 'No selection'
-
-
-@upgrade_step('provisionalClassification', '2', '3')
-def provisionalClassification_2_3(value, system):
-    # https://github.com/ClinGen/clincoded/issues/1196
     if 'replicatedOverTime' not in value:
         value['replicatedOverTime'] = False
 
     if 'contradictingEvidence' not in value:
-    	value['contradictingEvidence'] = {}
+        value['contradictingEvidence'] = {}
+        value['contradictingEvidence']['proband'] = False
+        value['contradictingEvidence']['caseControl'] = False
+        value['contradictingEvidence']['experimental'] = False
