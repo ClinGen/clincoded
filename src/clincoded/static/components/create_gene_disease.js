aaee5d2c7d46fb0da2878a31ba9ad370189f9f40
Jimmy Zhen <jzhen@stanford.edu>
2017-01-17 13:30:46 -0800

    Removed 'disabled' attribute for 'none' value select option associated with MOI adjective

--- src/clincoded/static/components/create_gene_disease.js
+++ src/clincoded/static/components/create_gene_disease.js
@@ -195,7 +195,7 @@ var CreateGeneDisease = React.createClass({
                                 <Input type="select" ref="moiAdjective" label="Select an adjective" defaultValue="none"
                                     error={this.getFormError('moiAdjective')} clearError={this.clrFormErrors.bind(null, 'moiAdjective')} inputDisabled={adjectiveDisabled}
                                     labelClassName="col-sm-5 control-label" wrapperClassName="col-sm-7" groupClassName="form-group" inputClassName="moiAdjective">
-                                    <option value="none" disabled="disabled">Select</option>
+                                    <option value="none">Select</option>
                                     <option disabled="disabled"></option>
                                     {adjectives.map(function(adjective, i) {
                                         return <option key={i} value={adjective}>{adjective.match(/^(.*?)(?: \(HP:[0-9]*?\)){0,1}$/)[1]}</option>;
