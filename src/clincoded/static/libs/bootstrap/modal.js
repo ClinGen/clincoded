69af9611f5d96d3b01b428502afa32eede23e553
Jimmy Zhen <jzhen@stanford.edu>
2017-02-07 15:38:28 -0800

    Updated code to replace modal mixin implementation

--- src/clincoded/static/libs/bootstrap/modal.js
+++ src/clincoded/static/libs/bootstrap/modal.js
@@ -2,6 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+// Display a modal dialog box that blocks all other page input until the user dismisses it. The
+// typical format looks like:
+//
+// <ModalComponent {...this.props}>
+//     // Render JSX...
+// </ModalComponent>
+//
+// This component has been rewritten due to the React v15.4.0 upgrade from v0.14.8.
+// The previous modal component used MixIn that resulted in the use of non-standard DOM attribute
+// when implementing the modal component along with the actuator (link/button to invoke the modal).
+// 'Unknown props warnings' are now flagged in React v15.4.0:
+// https://facebook.github.io/react/warnings/unknown-prop.html
+//
+// <ModalComponent> usage details:
+// See 'ModalComponent.propTypes' for details
+
+
 export default class ModalComponent extends React.Component {
     constructor(props) {
         super(props);
@@ -16,6 +34,7 @@ export default class ModalComponent extends React.Component {
         this.props.onRef(null);
     }
 
+    // Called by the actuator (link/button to invoke the modal)
     openModal() {
         this.setState({ isModalOpen: true });
 
@@ -23,6 +42,7 @@ export default class ModalComponent extends React.Component {
         document.body.classList.add('modal-open');
     }
 
+    // Called by the modal's Cancel' button defined in the parent component
     closeModal() {
         this.setState({ isModalOpen: false });
 
@@ -33,11 +53,15 @@ export default class ModalComponent extends React.Component {
     render() {
         return (
             <div className={this.props.modalWrapperClass}>
-                <a className={"btn btn-default " + this.props.actuatorClass} onClick={() => this.openModal()}>{this.props.actuatorTitle}</a>
+                {this.props.actuatorTitle ?
+                    <a className={"btn btn-default " + this.props.actuatorClass} onClick={() => this.openModal()}>{this.props.actuatorTitle}</a>
+                : null}
                 <Modal isOpen={this.state.isModalOpen}>
-                    <div className={"modal-header " + this.props.modalClass}>
-                        <h4 className="modal-title">{this.props.modalTitle}</h4>
-                    </div>
+                    {this.props.modalTitle ?
+                        <div className={"modal-header " + this.props.modalClass}>
+                            <h4 className="modal-title">{this.props.modalTitle}</h4>
+                        </div>
+                    : null}
                     {this.props.children}
                 </Modal>
             </div>
@@ -46,12 +70,12 @@ export default class ModalComponent extends React.Component {
 }
 
 ModalComponent.propTypes = {
-    modalTitle: React.PropTypes.string.isRequired, // Title in modal's header
+    modalTitle: React.PropTypes.string, // Title in modal's header
     modalClass: React.PropTypes.string, // CSS class for modal header
-    modalWrapperClass: React.PropTypes.string,
-    actuatorClass: React.PropTypes.string,
-    actuatorTitle: React.PropTypes.string,
-    children: React.PropTypes.node
+    modalWrapperClass: React.PropTypes.string, // CSS class for modal DOM wrapper
+    actuatorClass: React.PropTypes.string, // CSS class for link/button to invoke modal
+    actuatorTitle: React.PropTypes.string, // Text for link/button to invoke modal
+    children: React.PropTypes.node // JSX such as input field(s), dropdown(s), buttons
 };
 
 class Modal extends React.Component {
