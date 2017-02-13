1b58494c300865cb6f596f30e5cb10c810e2cd27
Karen Dalton <kdalton@stanford.edu>
2017-02-13 11:34:37 -0800

    add interpretation to doc_types, do not limit results if gdm or interp is queried

--- src/clincoded/search.py
+++ src/clincoded/search.py
@@ -334,7 +334,8 @@ def search(context, request, search_type=None):
             doc_types = []
         else:
             doc_types = ['gene', 'orphaPhenotype', 'article', 'variant', 'gdm', 'annotation',
-                         'group', 'family', 'individual', 'experimental', 'assessment']
+                         'group', 'family', 'individual', 'experimental', 'assessment',
+                         'interpretation']
     else:
         for item_type in doc_types:
             qs = urlencode([
@@ -388,7 +389,9 @@ def search(context, request, search_type=None):
 
     set_facets(facets, used_filters, query, principals)
 
-    if 'gdm' in doc_types or 'interpretation' in doc_types:
+    # if ('gdm' in doc_types or 'interpretation' in doc_types) and not (
+    #    'gene' in doc_types or 'disease' in doc_types):
+    if doc_types == ['gdm'] or doc_types == ['interpretation']:
         size = 99999
 
     # Execute the query
