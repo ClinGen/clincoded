c64e890adcbf69fb7a78487864c7082f9f4f3f45
Jimmy Zhen <jzhen@stanford.edu>
2017-02-13 17:02:36 -0800

    Updated pattern matching for ORPHA IDs so that only 'ORPHANNNNN' or 'ORPHA:NNNNNN' are accepted

--- src/clincoded/static/components/curator.js
+++ src/clincoded/static/components/curator.js
@@ -1395,7 +1395,7 @@ function captureBase(s, re, uppercase) {
 module.exports.capture = {
     // Find all the comma-separated 'orphaXX' occurrences. Return all valid orpha IDs in an array.
     orphas: function(s) {
-        return captureBase(s, /^\s*orpha(\d+)\s*$/i);
+        return captureBase(s, /^\s*orpha:?(\d+)\s*$/i);
     },
 
     // Find all the comma-separated gene-symbol occurrences. Return all valid symbols in an array.
