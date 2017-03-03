ba064b04571686a71d53603f5e63a977bc471767
Jimmy Zhen <jzhen@stanford.edu>
2017-03-03 13:27:54 -0800

    Addressed issue in which the 'Input' component going from unontrolled input of type text to be controlled

--- src/clincoded/static/components/variant_central/interpretation.js
+++ src/clincoded/static/components/variant_central/interpretation.js
@@ -418,7 +418,7 @@ var InterpretationCollection = module.exports.InterpretationCollection = React.c
                     <div className="col-sm-12 col-md-3">
                         <Form formClassName="form-std gdm-filter-form">
                             <Input type="text" ref="q" placeholder="Filter by Variant or Disease" handleChange={this.searchChange}
-                                labelClassName="control-label" groupClassName="form-group" />
+                                value={this.state.searchTerm} labelClassName="control-label" groupClassName="form-group" />
                         </Form>
                     </div>
                 </div>
