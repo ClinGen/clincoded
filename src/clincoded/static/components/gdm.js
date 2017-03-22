6860eb50daad891247823c6c937eca9d061ad602
Jimmy Zhen <jzhen@stanford.edu>
2017-03-21 21:14:09 -0700

    Trying other debugging

--- src/clincoded/static/components/gdm.js
+++ src/clincoded/static/components/gdm.js
@@ -158,11 +158,11 @@ var GdmCollectionRenderer = React.createClass({
                         var latestAnnotation = gdm && curator.findLatestAnnotation(gdm);
                         var mode = gdm.modeInheritance.match(/^(.*?)(?: \(HP:[0-9]*?\)){0,1}$/)[1];
                         let createdTimeStamp = new Date(gdm.date_created);
-                        let createdDate = moment(createdTimeStamp).format("YYYY MMM DD");
-                        let createdTime = moment(createdTimeStamp).format("h:mm a");
+                        let createdDate = moment(createdTimeStamp).format();
+                        let createdTime = moment(createdTimeStamp).format();
                         let latestTimeStamp = latestAnnotation ? new Date(latestAnnotation.date_created) : null;
-                        let latestDate = latestTimeStamp ? moment(latestTimeStamp).format("YYYY MMM DD") : '';
-                        let latestTime = latestTimeStamp ? moment(latestTimeStamp).format("h:mm a") : '';
+                        let latestDate = latestTimeStamp ? moment(latestTimeStamp).format() : '';
+                        let latestTime = latestTimeStamp ? moment(latestTimeStamp).format() : '';
                         var participants = annotationOwners.map(owner => { return owner.title; }).join(', ');
                         var statusString = statusMappings[gdm.gdm_status].cssClass; // Convert status string to CSS class
                         var iconClass = 'icon gdm-status-icon-' + statusString;
