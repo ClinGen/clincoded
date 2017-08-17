0d86221783dbfe083553e94e52b3d36ab54ee1fa
Jimmy Zhen <jzhen@stanford.edu>
2017-08-16 17:54:06 -0700

    Added/updated score configs for Experimental Rescue

--- src/clincoded/static/components/score/constants/score_maps.js
+++ src/clincoded/static/components/score/constants/score_maps.js
@@ -77,13 +77,23 @@ const SCORE_MAPS = {
         MAX_SCORE: 4
     },
     RESCUE_PATIENT_CELLS: {
+        DEFAULT_SCORE: 1,
+        SCORE_RANGE: [0, 0.5, 1, 1.5, 2],
+        MAX_SCORE: 4
+    },
+    RESCUE_CELL_CULTURE_MODEL: {
+        DEFAULT_SCORE: 1,
+        SCORE_RANGE: [0, 0.5, 1, 1.5, 2],
+        MAX_SCORE: 4
+    },
+    RESCUE_ANIMAL_MODEL: {
         DEFAULT_SCORE: 2,
         SCORE_RANGE: [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
         MAX_SCORE: 4
     },
-    RESCUE_ENGINEERED_EQUIVALENT: {
-        DEFAULT_SCORE: 1,
-        SCORE_RANGE: [0, 0.5, 1, 1.5, 2],
+    RESCUE_PATIENT_HUMAN_MODEL: {
+        DEFAULT_SCORE: 2,
+        SCORE_RANGE: [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
         MAX_SCORE: 4
     }
 };
