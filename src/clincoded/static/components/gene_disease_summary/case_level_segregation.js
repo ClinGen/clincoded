092d270ea2afc7030dcb7ec076629bb1f75f35e8
Jimmy Zhen <jzhen@stanford.edu>
2017-10-03 20:10:19 -0700

    #1411 Updated segregation evidence data table rendering

--- src/clincoded/static/components/gene_disease_summary/case_level_segregation.js
+++ src/clincoded/static/components/gene_disease_summary/case_level_segregation.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { external_url_map } from '../globals';
 
 class GeneDiseaseEvidenceSummarySegregation extends Component {
     constructor(props) {
@@ -24,20 +25,32 @@ class GeneDiseaseEvidenceSummarySegregation extends Component {
     renderSegregationEvidence(evidence, key) {
         return (
             <tr key={key} className="scored-segregation-evidence">
+                <td className="evidence-reference">
+                    <span>{evidence.authors.join(', ')}, <strong>{evidence.pubYear}</strong>, <a href={external_url_map['PubMed'] + evidence.pmid} target="_blank">PMID: {evidence.pmid}</a></span>
+                </td>
+                <td className="evidence-ethnicity">
+                    {evidence.ethnicity}
+                </td>
+                <td className="evidence-phenotypes">
+                    {evidence.hpoIdInDiagnosis.length ? <span><strong>HPO term(s):</strong> {evidence.hpoIdInDiagnosis.join(', ')}</span> : null}
+                    {evidence.hpoIdInDiagnosis.length && evidence.termsInDiagnosis.length ? <span>; </span> : null}
+                    {evidence.termsInDiagnosis.length ? <span><strong>free text:</strong> {evidence.termsInDiagnosis}</span> : null}
+                </td>
                 <td className="evidence-segregation-num-affected">
                     {evidence.segregationNumAffected}
                 </td>
                 <td className="evidence-segregation-num-unaffected">
                     {evidence.segregationNumUnaffected}
                 </td>
-                <td className="evidence-published-lod-score">
-                    {evidence.segregationPublishedLodScore}
-                </td>
-                <td className="evidence-estimated-lod-score">
-                    {evidence.segregationEstimatedLodScore}
+                <td className="evidence-lod-score">
+                    {evidence.segregationPublishedLodScore ?
+                        <span><strong>Published:</strong> {evidence.segregationPublishedLodScore}</span>
+                        : 
+                        (evidence.segregationEstimatedLodScore ? <span><strong>Calculated:</strong> {evidence.segregationEstimatedLodScore}</span> : '-')
+                    }
                 </td>
                 <td className="evidence-lod-score-counted">
-                    {evidence.includeLodScoreInAggregateCalculation ? 'Yes' : 'No'}
+                    {evidence.segregationPublishedLodScore || evidence.segregationEstimatedLodScore ? <span>{evidence.includeLodScoreInAggregateCalculation ? 'Yes' : 'No'}</span> : '-'}
                 </td>
             </tr>
         );
@@ -48,19 +61,21 @@ class GeneDiseaseEvidenceSummarySegregation extends Component {
         let self = this;
 
         return (
-            <div className="evidence-summary panel-experimental">
+            <div className="evidence-summary panel-case-level-segregation">
                 <div className="panel panel-info">
                     <div className="panel-heading">
-                        <h3 className="panel-title">Genetic Evidence: Case Level (segregation without proband)</h3>
+                        <h3 className="panel-title">Genetic Evidence: Case Level (family segregation information without proband data)</h3>
                     </div>
                     {segregationEvidenceList && segregationEvidenceList.length ?
                         <table className="table">
                             <thead>
                                 <tr>
+                                    <th>Reference</th>
+                                    <th>Family ethnicity</th>
+                                    <th>Family phenotypes</th>
                                     <th>Number of affected individuals</th>
                                     <th>Number of unaffected individuals</th>
-                                    <th>Published LOD score</th>
-                                    <th>Estimated LOD score</th>
+                                    <th>LOD score</th>
                                     <th>LOD score counted</th>
                                 </tr>
                             </thead>
@@ -72,7 +87,7 @@ class GeneDiseaseEvidenceSummarySegregation extends Component {
                         </table>
                         :
                         <div className="panel-body">
-                            <span>No scored Experimental evidence was found.</span>
+                            <span>No segregation evidence for a Family without a proband was found.</span>
                         </div>
                     }
                 </div>
