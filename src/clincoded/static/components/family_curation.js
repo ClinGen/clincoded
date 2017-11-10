8ad00d5fa80db51c57c14f79f828cfbbbbeff970
Jimmy Zhen <jzhen@stanford.edu>
2017-11-09 19:36:43 -0800

    Replaced fontawesome icon with 'Note:' text string in LOD score note

--- src/clincoded/static/components/family_curation.js
+++ src/clincoded/static/components/family_curation.js
@@ -1634,7 +1634,7 @@ function FamilySegregation() {
             </Input>
             <div className="col-sm-7 col-sm-offset-5 lod-score-inclusion-note">
                 <p className="alert alert-warning">
-                    <i className="icon icon-exclamation-circle"></i> For X-linked and autosomal dominant conditions, only include families with 4 or more segregations in the final calculation.
+                    Note: For X-linked and autosomal dominant conditions, only include families with 4 or more segregations in the final calculation.
                     For autosomal recessive conditions, only include families with at least 3 affected individuals. See the Gene Curation SOP for additional details.
                 </p>
             </div>
