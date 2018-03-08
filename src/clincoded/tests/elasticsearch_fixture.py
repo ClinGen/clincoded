c214b31b583b6816c35e896e81f5f3303f6d1f48
Chris <gto389ho@gmail.com>
2018-03-08 11:08:05 -0800

    Update to sudo command

--- src/clincoded/tests/elasticsearch_fixture.py
+++ src/clincoded/tests/elasticsearch_fixture.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     import subprocess
 
-sudoCommand = "sudo"
+sudoCommand = ["sudo"]
 
 def server_process(datadir, host='127.0.0.1', port=9200, prefix='', echo=False):
     args = [
