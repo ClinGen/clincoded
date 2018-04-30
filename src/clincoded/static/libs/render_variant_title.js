50fa58d22b716a75667c80d0bed0ffc249b45ee6
Jimmy Zhen <jzhen@stanford.edu>
2018-04-30 10:55:44 -0700

    :tada: Initial commit of shared lib function to render variant title

---
+++ src/clincoded/static/libs/render_variant_title.js
@@ -0,0 +1,29 @@
+'use strict';
+import React from 'react';
+
+/**
+ * Method to display the title of a variant.
+ * 1st option is the ClinVar Preferred Title if it's present.
+ * 2nd alternative is a string constructed with the canonical transcript, gene symbol and protein change.
+ * The fallback is the GRCh38 NC_ HGVS name.
+ * @param {object} variant - A variant object
+ */
+export function renderVariantTitle(variant) {
+    let variantTitle;
+    if (variant) {
+        if (variant.clinvarVariantId && variant.clinvarVariantTitle) {
+            variantTitle = variant.clinvarVariantTitle;
+        } else if (variant.carId && variant.canonicalTranscriptTitle) {
+            variantTitle = variant.canonicalTranscriptTitle;
+        } else if (variant.hgvsNames && Object.keys(variant.hgvsNames).length && !variantTitle) {
+            if (variant.hgvsNames.GRCh38) {
+                variantTitle = variant.hgvsNames.GRCh38 + ' (GRCh38)';
+            } else if (variant.hgvsNames.GRCh37) {
+                variantTitle = variant.hgvsNames.GRCh37 + ' (GRCh37)';
+            } else {
+                variantTitle = variant.carId ? variant.carId : 'A preferred title is not available';
+            }
+        }
+    }
+    return <span className="variant-title">{variantTitle}</span>;
+}
