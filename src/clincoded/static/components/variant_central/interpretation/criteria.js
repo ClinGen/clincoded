ae9d440066df59e015eaad013d7a644f8a6b8ad4
Jimmy Zhen <jzhen@stanford.edu>
2018-05-04 11:44:24 -0700

    Implement functionality to allow users to click any given criteria buttons to be taken to the targeted evaluation form on the expected tab (and subtab)

--- src/clincoded/static/components/variant_central/interpretation/criteria.js
+++ src/clincoded/static/components/variant_central/interpretation/criteria.js
@@ -2,30 +2,82 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
+import { queryKeyValue, editQueryValue, addQueryKey } from '../../globals';
 
-var evidenceCodes = require('./mapping/evidence_code.json');
+const evidenceCodes = require('./mapping/evidence_code.json');
+
+const mappingTab = {
+    population: 'population',
+    predictors: 'predictors',
+    computational: 'predictors',
+    functional: 'experimental',
+    experimental: 'experimental',
+    segregation: 'segregation-case'
+};
+
+const mappingSubtab = {
+    BP1: 'missense',
+    BP4: 'missense',
+    PP2: 'missense',
+    PP3: 'missense',
+    PM5: 'missense',
+    PS1: 'missense',
+    PVS1: 'lof',
+    BP7: 'silent-intron',
+    BP3: 'indel',
+    PM4: 'indel'
+};
 
 // Display met criteria
 var CurationInterpretationCriteria = module.exports.CurationInterpretationCriteria = createReactClass({
     propTypes: {
         interpretation: PropTypes.object,
-        selectedTab: PropTypes.string
+        selectedTab: PropTypes.string,
+        updateSelectedCriteria: PropTypes.func
     },
 
-    getInitialState: function() {
+    getInitialState() {
         return {
-            interpretation: this.props.interpretation,
             selectedTab: this.props.selectedTab
         };
     },
 
-    componentWillReceiveProps: function(nextProps) {
-        if (nextProps.interpretation && this.props.interpretation) {
-            this.setState({interpretation: nextProps.interpretation});
-        }
+    componentWillReceiveProps(nextProps) {
         if (nextProps.selectedTab) {
             this.setState({selectedTab: nextProps.selectedTab});
         }
+    },
+
+    /**
+     * Method to handle click events on the criteria buttons
+     * Should take users to the tab of the targeted criteriaz
+     */
+    handleClick(key, category, e) {
+        e.preventDefault(); e.stopPropagation();
+
+        const interpretation = this.props.interpretation,
+                selectedTab = this.state.selectedTab,
+                selectedSubtab = queryKeyValue('subtab', window.location.href),
+                selectedCriteria = queryKeyValue('criteria', window.location.href);
+
+        if (interpretation) {
+            if (selectedTab) {
+                window.history.replaceState(window.state, '', editQueryValue(window.location.href, 'tab', mappingTab[category]));
+            } else {
+                window.history.replaceState(window.state, '', addQueryKey(window.location.href, 'tab', mappingTab[category]));
+            }
+            if (selectedSubtab) {
+                window.history.replaceState(window.state, '', editQueryValue(window.location.href, 'subtab', mappingSubtab[key]));
+            } else {
+                window.history.replaceState(window.state, '', addQueryKey(window.location.href, 'subtab', mappingSubtab[key]));
+            }
+            if (selectedCriteria) {
+                window.history.replaceState(window.state, '', editQueryValue(window.location.href, 'criteria', key));
+            } else {
+                window.history.replaceState(window.state, '', addQueryKey(window.location.href, 'criteria', key));
+            }
+        }
+        this.props.updateSelectedCriteria(mappingTab[category], mappingSubtab[key], key);
     },
 
     // Method to render individual criteria codes and their respective tooltip
@@ -47,7 +99,9 @@ var CurationInterpretationCriteria = module.exports.CurationInterpretationCriter
             <button className={'btn btn-default ' + evidence[key].class + ' ' + evidence[key].category
                 + this.getCurrentTab(evidence[key].category)}
                 type="button" key={key} data-status={status} data-
-                data-toggle="tooltip" data-placement="top" data-tooltip={evidence[key].definition}>
+                data-toggle="tooltip" data-placement="top"
+                data-tooltip={evidence[key].definition}
+                onClick={this.handleClick.bind(this, key, evidence[key].category)}>
                 <span>{key}</span>
             </button>
         );
@@ -82,12 +136,12 @@ var CurationInterpretationCriteria = module.exports.CurationInterpretationCriter
     },
 
     render() {
-        var keys = Object.keys(evidenceCodes);
-        var interpretation = this.state.interpretation;
+        const keys = Object.keys(evidenceCodes);
+        const { interpretation } = this.props;
 
         return (
             <div className="container curation-criteria curation-variant">
-                {(interpretation) ?
+                {interpretation ?
                     <div className="criteria-bar btn-toolbar" role="toolbar" aria-label="Criteria bar with code buttons">
                         <div className="criteria-group btn-group btn-group-sm" role="group" aria-label="Criteria code button group">
                             {keys.map(key => {
