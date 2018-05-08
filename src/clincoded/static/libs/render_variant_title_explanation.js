7788c984c07d81af3f819a93dac8f663f93063b8
Jimmy Zhen <jzhen@stanford.edu>
2018-05-07 22:05:50 -0700

    Update variant title explanation text

--- src/clincoded/static/libs/render_variant_title_explanation.js
+++ src/clincoded/static/libs/render_variant_title_explanation.js
@@ -6,7 +6,7 @@ import { ContextualHelp } from './bootstrap/contextual_help';
  * Method to render a mouseover explanation for the variant title
  */
 export function renderVariantTitleExplanation() {
-    const explanation = 'The transcript with the longest translation with no stop codons. If no translation, then the longest non-protein-coding transcript.';
+    const explanation = 'For ClinVar alleles, this represents the ClinVar Preferred Title. For alleles not in ClinVar, this HGVS is based on the transcript with the longest translation with no stop codons or, if no translation, the longest non-protein-coding transcript. If a single canonical transcript is not discernible the HGVS is based on the GRCh38 genomic coordinates.';
     return (
         <span className="variant-title-explanation"><ContextualHelp content={explanation} /></span>
     );
