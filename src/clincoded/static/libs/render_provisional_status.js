2717306957f7bc543e164f1c49e3af3cba5d112e
Jimmy Zhen <jzhen@stanford.edu>
2018-09-07 16:27:49 -0700

    :tada: Initial commit for function that renders a 'PROVISIONAL' status label for a given classification

---
+++ src/clincoded/static/libs/render_provisional_status.js
@@ -0,0 +1,45 @@
+'use strict';
+import React from 'react';
+import moment from 'moment';
+import { sortListByDate } from './helpers/sort';
+
+/**
+ * Method to render the provisional status of a given GDM's classification
+ * @param {array} snapshots - List of snapshots associated with classification
+ * @param {object} gdm - The GDM object
+ * @param {object} context - The global context object
+ * @param {boolean} allowApproval - Whether to render link for view/approve provisional
+ */
+export function renderProvisionalStatus(snapshots, gdm, context, allowApproval) {
+    const sortedSnapshots = snapshots && snapshots.length ? sortListByDate(snapshots, 'date_created') : [];
+    // Get any snapshots that had been provisioned
+    const provisionedSnapshots = sortedSnapshots.filter(snapshot => {
+        return snapshot.approvalStatus === 'Provisioned' && snapshot.resourceType === 'classification';
+    });
+    // Get any snapshots that had been approved
+    const approvedSnapshots = sortedSnapshots.filter(snapshot => {
+        return snapshot.approvalStatus === 'Approved' && snapshot.resourceType === 'classification';
+    });
+
+    if (provisionedSnapshots && provisionedSnapshots.length && (!approvedSnapshots || (approvedSnapshots && !approvedSnapshots.length))) {
+        return (
+            <span className="provisional-status-wrapper">
+                <span className="label label-info status-item" data-toggle="tooltip" data-placement="top"
+                    data-tooltip={'Provisioned on ' + moment(provisionedSnapshots[0].date_created).format("YYYY MMM DD, h:mm a")}>
+                    PROVISIONAL
+                </span>
+                {context && context.name === 'curation-central' && allowApproval ? renderProvisionalLink(gdm) : null}
+            </span>
+        );
+    } else {
+        return null;
+    }
+}
+
+function renderProvisionalLink(gdm) {
+    return (
+        <span className="classification-link-item">
+            <a href={'/provisional-classification/?gdm=' + gdm.uuid + '&approval=yes'} title="View/Approve Current Provisional"><i className="icon icon-link"></i></a>
+        </span>
+    );
+}
