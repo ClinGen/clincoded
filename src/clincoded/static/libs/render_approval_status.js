4eed2919fa3cd2f2837b3b8488eef9ae6d7c070e
Jimmy Zhen <jzhen@stanford.edu>
2018-09-11 00:23:33 -0700

    Added 'resourceType' parameter for handling both GCI classification and VCI interpretation

--- src/clincoded/static/libs/render_approval_status.js
+++ src/clincoded/static/libs/render_approval_status.js
@@ -4,17 +4,18 @@ import moment from 'moment';
 import { sortListByDate } from './helpers/sort';
 
 /**
- * Method to render the approval status of a given GDM's classification
- * @param {array} snapshots - List of snapshots associated with classification
+ * Method to render the 'APPROVED' status of a given GDM's classification or interpretation
+ * @param {array} snapshots - List of snapshots associated with a classification or interpretation
+ * @param {string} resourceType - A string value of either 'classification' or 'interpretation'
  * @param {object} context - The global context object
  * @param {string} affiliationId - The affiliation ID
  * @param {string} userId - The user's UUID
  */
-export function renderApprovalStatus(snapshots, context, affiliationId, userId) {
+export function renderApprovalStatus(snapshots, resourceType, context, affiliationId, userId) {
     const sortedSnapshots = snapshots && snapshots.length ? sortListByDate(snapshots, 'date_created') : [];
     // Get any snapshots that had been approved
     const approvedSnapshots = sortedSnapshots.filter(snapshot => {
-        return snapshot.approvalStatus === 'Approved' && snapshot.resourceType === 'classification';
+        return snapshot.approvalStatus === 'Approved' && snapshot.resourceType === resourceType;
     });
 
     if (approvedSnapshots && approvedSnapshots.length) {
@@ -24,8 +25,8 @@ export function renderApprovalStatus(snapshots, context, affiliationId, userId) 
                     data-tooltip={'Approved on ' + moment(approvedSnapshots[0].date_created).format("YYYY MMM DD, h:mm a")}>
                     APPROVED
                 </span>
-                {context && context.name.match(/curation-central|provisional-curation|provisional-classification/) ?
-                    renderApprovalLink(approvedSnapshots[0], affiliationId, userId)
+                {(resourceType === 'classification' && context && context.name.match(/curation-central|provisional-curation|provisional-classification/)) || resourceType === 'interpretation' ?
+                    renderApprovalLink(approvedSnapshots[0], resourceType, affiliationId, userId)
                     : null}
             </span>
         );
@@ -35,24 +36,30 @@ export function renderApprovalStatus(snapshots, context, affiliationId, userId) 
 }
 
 /**
- * Method to render linkout to the evidence summary of a given approved classification
- * @param {object} snapshot - The approved classification snapshot
+ * Method to render linkout to the evidence summary of a given approved classification or interpretation
+ * @param {object} snapshot - The approved classification or interpretation snapshot
+ * @param {string} resourceType - A string value of either 'classification' or 'interpretation'
  * @param {string} affiliationId - The affiliation ID
  * @param {string} userId - The user's UUID
  */
-function renderApprovalLink(snapshot, affiliationId, userId) {
-    let url, param = '';
+function renderApprovalLink(snapshot, resourceType, affiliationId, userId) {
+    let url, param = '', summary_uri = '';
+    if (resourceType === 'classification') {
+        summary_uri = '/gene-disease-evidence-summary/';
+    } else if (resourceType === 'interpretation') {
+        summary_uri = '/variant-interpretation-summary/';
+    }
     if (snapshot) {
         if (affiliationId && affiliationId.length) {
             param = '&status=Approved&affiliationId=' + affiliationId;
         } else if (userId && userId.length) {
             param = '&status=Approved&userId=' + userId;
         }
-        url = '/gene-disease-evidence-summary/?snapshot=' + snapshot.uuid + param;
+        url = summary_uri + '?snapshot=' + snapshot.uuid + param;
     }
     return (
         <span className="classification-link-item">
-            <a href={url} title="View Current Approved Classification" target="_blank"><i className="icon icon-link"></i></a>
+            <a href={url} title="View Current Approved" target="_blank"><i className="icon icon-link"></i></a>
         </span>
     );
 }
