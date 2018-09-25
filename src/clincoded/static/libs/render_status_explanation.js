eca6f97f1a0b0adfb11acacaf1e815697b66bb55
Jimmy Zhen <jzhen@stanford.edu>
2018-09-24 22:00:31 -0700

    Updated shared lib function to accept a parameter so that it can be used by both classification and interpretation

--- src/clincoded/static/libs/render_status_explanation.js
+++ src/clincoded/static/libs/render_status_explanation.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { ContextualHelp } from './bootstrap/contextual_help';
 
 /**
- * Method to render a mouseover explanation for the variant title
+ * Method to render a mouseover explanation for the all classifications and interpretations panel titles
+ * @param {string} resourceType - A text string of either 'Classifications' or 'Interpretations'
  */
-export function renderInterpretationStatusExplanation() {
-    const explanation = 'Interpretations marked "Approved" may be viewed by any user within the interface; those marked "In progress" or "Provisional" are viewable only by the submitter.';
+export function renderStatusExplanation(resourceType) {
+    const explanation = resourceType + ' marked as "Approved" may be viewed by any user within the interface; those marked as "In progress" or "Provisional" are viewable only by the submitter.';
     return (
         <span className="interpretation-status-explanation"><ContextualHelp content={explanation} /></span>
     );
