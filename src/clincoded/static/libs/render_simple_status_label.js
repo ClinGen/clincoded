7a0350858e1b939404c32459537e71cd8afd654a
Jimmy Zhen <jzhen@stanford.edu>
2018-09-25 23:47:22 -0700

    Updated function as a result of resolving merge conflict

--- src/clincoded/static/libs/render_simple_status_label.js
+++ src/clincoded/static/libs/render_simple_status_label.js
@@ -5,13 +5,19 @@ import React from 'react';
  * Method to generically display a status tag/label given the status of a classification
  * Applicable to: snapshots, GCI Evidence Summary, VCI Evaluation Summary
  * @param {string} status - The status of a given classification in a GDM or Interpretation
+ * @param {boolean} publishStatus - The publication status of a given classification
  */
-export function renderSimpleStatusLabel(status) {
+export function renderSimpleStatusLabel(status, publishStatus) {
     if (status === 'In progress') {
         return <span className="label label-warning">IN PROGRESS</span>;
     } else if (status.match(/Provisional|Provisioned/)) {
         return <span className="label label-info">PROVISIONAL</span>;
     } else if (status === 'Approved') {
-        return <span className="label label-success">APPROVED</span>;
+        return (
+            <span>
+                <span className="label label-success">APPROVED</span>
+                {publishStatus ? <span className="label publish-background">PUBLISHED</span> : null}
+            </span>
+        );
     }
 }
