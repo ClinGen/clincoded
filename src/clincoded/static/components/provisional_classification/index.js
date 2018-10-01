bfe7ddca9962c73e8cc7653609fe568ea12edbe3
Jimmy Zhen <jzhen@stanford.edu>
2018-09-30 20:25:58 -0700

    Generic message text update

--- src/clincoded/static/components/provisional_classification/index.js
+++ src/clincoded/static/components/provisional_classification/index.js
@@ -623,7 +623,7 @@ const ProvisionalClassification = createReactClass({
                                 <div className="container">
                                     <p className="alert alert-info">
                                         <i className="icon icon-info-circle"></i> The option to publish an approved classification is unavailable when any of the following
-                                            apply: 1) your affiliation does not have permission to publish, 2) the mode of inheritance Is not supported by the Clinical Validity
+                                            apply: 1) your affiliation does not have permission to publish, 2) the mode of inheritance is not supported by the Clinical Validity
                                             Classification framework, 3) the associated disease does not have a MONDO ID, 4) it is based on a previous version of the SOP.
                                     </p>
                                 </div>
--- src/clincoded/static/components/provisional_curation.js
+++ src/clincoded/static/components/provisional_curation.js
@@ -1194,7 +1194,7 @@ var ProvisionalCuration = createReactClass({
                                     <div>
                                         <p className="alert alert-info">
                                             <i className="icon icon-info-circle"></i> The option to publish an approved classification is unavailable when any of the following
-                                                apply: 1) your affiliation does not have permission to publish, 2) the mode of inheritance Is not supported by the Clinical Validity
+                                                apply: 1) your affiliation does not have permission to publish, 2) the mode of inheritance is not supported by the Clinical Validity
                                                 Classification framework, 3) the associated disease does not have a MONDO ID, 4) it is based on a previous version of the SOP.
                                         </p>
                                     </div>
