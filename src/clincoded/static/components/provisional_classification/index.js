409fa2a48497d020a8be73243eee8961a0ea5bbb
Jimmy Zhen <jzhen@stanford.edu>
2018-09-30 18:10:03 -0700

    Add MOI and MONDO ID as conditions to determine whether publishing is allowed
    And display a generic message when publishing is not allowed

--- src/clincoded/static/components/provisional_classification/index.js
+++ src/clincoded/static/components/provisional_classification/index.js
@@ -263,8 +263,12 @@ const ProvisionalClassification = createReactClass({
      */
     isUserAllowedToPublish() {
         const curatorAffiliation = this.props.affiliation;
+        const gdm = this.state.gdm;
+        let validModeInheritance = gdm && gdm.modeInheritance &&
+            gdm.modeInheritance.match(/Autosomal dominant inheritance (HP:0000006)|Autosomal recessive inheritance (HP:0000007)|X-linked inheritance (HP:0001417)/);
+        let hasMondoId = gdm && gdm.disease && gdm.disease.diseaseId && gdm.disease.diseaseId.indexOf('MONDO') > -1;
 
-        if (curatorAffiliation && curatorAffiliation.publish_approval) {
+        if (curatorAffiliation && curatorAffiliation.publish_approval && validModeInheritance && hasMondoId) {
             return true;
         } else {
             return false;
@@ -614,6 +618,15 @@ const ProvisionalClassification = createReactClass({
                                             </Panel>
                                         </PanelGroup>
                                     </div>
+                                </div>
+                                : null}
+                            {!this.state.showPublish || !this.isUserAllowedToPublish() ?
+                                <div className="container">
+                                    <p className="alert alert-info">
+                                        <i className="icon icon-info-circle"></i> The option to publish an approved classification is unavailable when any of the following
+                                            apply: 1) your affiliation does not have permission to publish, 2) the mode of inheritance Is not supported by the Clinical Validity
+                                            Classification framework, 3) the associated disease does not have a MONDO ID, 4) it is based on a previous version of the SOP.
+                                    </p>
                                 </div>
                                 : null}
                             {sortedSnapshotList.length ?
