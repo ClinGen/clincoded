27fe3dc4e7c888922017b8a09296e1b7c0844115
Jimmy Zhen <jzhen@stanford.edu>
2018-12-06 11:32:10 -0800

    Added the UIs for CIViC linkout and changed 'createReactClass' to class component

--- src/clincoded/static/components/variant_central/record_variant.js
+++ src/clincoded/static/components/variant_central/record_variant.js
@@ -1,61 +1,51 @@
 'use strict';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import createReactClass from 'create-react-class';
 import { external_url_map } from '../globals';
 
 // Display the curator data of the curation data
-var CurationRecordVariant = module.exports.CurationRecordVariant = createReactClass({
-    propTypes: {
-        data: PropTypes.object, // ClinVar data payload
-        interpretationTranscript: PropTypes.string,
-        updateInterpretationTranscript: PropTypes.func
-    },
+class CurationRecordVariant extends Component {
+    render() {
+        const { data } = this.props;
+        let clinVarId, carId, dbSNPId;
 
-    getDefaultProps: function() {
-        return {
-            recordHeader: 'Variant ID Sources'
-        };
-    },
-
-    render: function() {
-        var variant = this.props.data;
-        var recordHeader = this.props.recordHeader;
-        if (variant) {
-            var clinVarId = (variant.clinvarVariantId) ? variant.clinvarVariantId : null;
-            var carId = (variant.carId) ? variant.carId : null;
-            var dbSNPId = (variant.dbSNPIds.length) ? variant.dbSNPIds[0] : null;
-            if (dbSNPId && dbSNPId.indexOf('rs') == -1) {
+        if (data) {
+            clinVarId = (data.clinvarVariantId) ? data.clinvarVariantId : null;
+            carId = (data.carId) ? data.carId : null;
+            dbSNPId = (data.dbSNPIds.length) ? data.dbSNPIds[0] : null;
+            if (dbSNPId && dbSNPId.indexOf('rs') < 0) {
                 dbSNPId = 'rs' + dbSNPId;
             }
         }
-        var addEdit = this.props.interpretationTranscript ? 'Edit' : 'Add';
 
         return (
             <div className="col-xs-12 col-sm-3 gutter-exc">
                 <div className="curation-data-gene">
-                    <h4>{recordHeader}</h4>
+                    <h4>Variant ID Sources</h4>
                     {clinVarId || carId ?
                         <dl className="inline-dl clearfix">
                             {clinVarId ?
                                 <dd>ClinVar VariationID:&nbsp;<a href={external_url_map['ClinVarSearch'] + clinVarId} target="_blank" title={'ClinVar page for ' + clinVarId + ' in a new window'}>{clinVarId}</a></dd>
-                                :
-                                null
-                            }
+                                : null}
                             {carId ?
                                 <dd>ClinGen Allele Registry ID:&nbsp;<a href={'http://reg.genome.network/allele/' + carId + '.html'} target="_blank" title={'GlinGen Allele Registry page for ' + carId + ' in a new window'}>{carId}</a></dd>
-                                :
-                                null
-                            }
+                                : null}
+                            {carId ?
+                                <dd>CIViC:&nbsp;<a href={external_url_map['CIViC'] + carId} target="_blank" title={'CIViC for ' + carId + ' in a new window'}>{carId}</a></dd>
+                                : null}
                             {dbSNPId ?
                                 <dd>dbSNP ID:&nbsp;<a href={'https://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=' + dbSNPId.replace('rs', '')} target="_blank" title={'dbSNP page for ' + dbSNPId + ' in a new window'}>{dbSNPId}</a></dd>
-                                :
-                                null
-                            }
+                                : null}
                         </dl>
-                    : null}
+                        : null}
                 </div>
             </div>
         );
     }
-});
+}
+
+CurationRecordVariant.propTypes = {
+    data: PropTypes.object, // Variant data
+}
+
+export default CurationRecordVariant;
