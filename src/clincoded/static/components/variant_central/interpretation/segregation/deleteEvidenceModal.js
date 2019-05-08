46a68b1f83fe116ad0bd57e7e0b03deaad7cd3d6
Gloria Cheung <gcheung@stanford.edu>
2019-05-08 11:16:32 -0700

    Moved confirm delete case segregation evidence modal to new file.

---
+++ src/clincoded/static/components/variant_central/interpretation/segregation/deleteEvidenceModal.js
@@ -0,0 +1,82 @@
+'use strict';
+  
+// stdlib
+import React from 'react';
+import createReactClass from 'create-react-class';
+import PropTypes from 'prop-types';
+
+// third party lib
+import { Input } from 'libs/bootstrap/form';
+import ModalComponent from 'libs/bootstrap/modal';
+
+/**
+ * Modal to confirm before deleting an evidence.
+ * Mainly for case segregation tab.
+ */
+let DeleteEvidenceModal = createReactClass({
+    propTypes: {
+        row: PropTypes.object,          // Selected evidence row to be deleted
+        useIcon: PropTypes.bool,        // Use icon as button title
+        deleteEvidence: PropTypes.func  // Function to call to delete the evidence row
+    },
+
+    getInitialState() {
+        return {
+            row: this.props.row,
+        }
+    },
+    
+    handleModalClose() {
+        this.child.closeModal();
+    },
+
+    cancel() {
+        this.handleModalClose();
+        this.setState({
+            row: this.props.row
+        });
+    },
+
+    /**
+     * Confirm button is clicked.  Delete given evidence row
+     */
+    clickConfirm() {
+        this.props.deleteEvidence(this.state.row);
+        this.handleModalClose();
+        this.setState({
+            row: null
+        });
+    },
+    
+    render() {
+        const title = this.props.useIcon ? <i className="icon icon-trash-o"></i> : "Delete";
+        const modalWrapperClass = this.props.useIcon ? "input-inline pull-right" : "";
+	    const bootstrapBtnClass = this.props.useIcon ? "btn pull-right" : "btn btn-danger ";
+        return (
+            <ModalComponent
+                    modalTitle="Confirm evidence deletion"
+                    modalClass="modal-default"
+                    modalWrapperClass={modalWrapperClass}
+                    bootstrapBtnClass={bootstrapBtnClass}
+                    actuatorTitle={title}
+                    onRef={ref => (this.child = ref)}
+                >
+                <div>
+                    <div className="modal-body">
+                        <p>
+                            Are you sure you want to delete this evidence?
+                        </p>
+                    </div>
+                    <div className='modal-footer'>
+                        <Input type="button" inputClassName="btn-primary btn-inline-spacer" clickHandler={this.clickConfirm} title="Confirm" />
+                        <Input type="button" inputClassName="btn-default btn-inline-spacer" clickHandler={this.cancel} title="Cancel" />
+                    </div>
+                </div>
+            </ModalComponent>
+        );
+    }
+});
+
+module.exports = {
+    DeleteEvidenceModal: DeleteEvidenceModal
+}
