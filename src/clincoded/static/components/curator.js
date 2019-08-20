11d8dbe7fc94a530d17d38da53088a346de51fbb
Mark Mandell <markmand@stanford.edu>
2019-08-20 16:38:43 -0700

    add provisionalClass props to flattenProvisional

--- src/clincoded/static/components/curator.js
+++ src/clincoded/static/components/curator.js
@@ -2231,7 +2231,7 @@ function flattenAssessment(assessment) {
 var provisionalSimpleProps = [
     "date_created", "classificationPoints", "replicatedOverTime", "contradictingEvidence", "autoClassification", "alteredClassification",
     "classificationStatus", "evidenceSummary", "reasons", "active", "affiliation", "classificationDate", "approvalSubmitter",
-    "classificationApprover", "approvalDate", "approvalReviewDate", "approvalComment", "provisionalSubmitter", "provisionalDate",
+    "classificationApprover", "approvalDate", "approvalReviewDate", "approvalComment", "curationApprovers", "curationContributors", "contributorComment", "contributorDate", "provisionalSubmitter", "provisionalDate",
     "provisionalReviewDate", "provisionalComment", "publishSubmitter", "publishAffiliation", "publishDate", "publishComment",
     "provisionedClassification", "approvedClassification", "publishClassification", "associatedClassificationSnapshots"
 ];
