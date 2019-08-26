e9ac5feafbc1698e4d8689d2a4a5fcb6f2ed57e9
Gloria Cheung <gcheung@stanford.edu>
2019-08-26 16:28:28 -0700

    Added codes to migrate existing case-segretation with new schema.

--- src/clincoded/upgrade/extra_evidence.py
+++ src/clincoded/upgrade/extra_evidence.py
@@ -18,6 +18,24 @@ def extra_evidence_2_3(value, system):
 @upgrade_step('extra_evidence', '3', '4')
 def extra_evidence_3_4(value, system):
     # https://github.com/ClinGen/clincoded/issues/1755
+    # Import uuidpmid dictionary data file
+    from clincoded.upgrade.uuidpmid import uuidList
     # Add source property
-    return
-
+    if 'category' in value:
+        if value['category'] == 'case-segregation':
+            value['source'] = {}
+            value['source']['data'] = {}
+            value['source']['metadata'] = {}
+            value['source']['relevant_criteria'] = []
+            value['source']['metadata']['_kind_key'] = 'PMID'
+            value['source']['metadata']['_kind_title'] = 'Pubmed'
+            # Since articles[] contains article uuid, get its pmid from array
+            # If uuid is not found, do not set pmid and ignore the error
+            try:
+                value['source']['metadata']['pmid'] = uuidList[value['articles'][0]]
+            except Exception:
+                pass
+            value['source']['data']['relevant_criteria'] = ''
+            value['source']['data']['comments'] = value['evidenceDescription']
+            if value['evidenceCriteria'] != 'none':
+                value['source']['data']['relevant_criteria'] = value['evidenceCriteria']
