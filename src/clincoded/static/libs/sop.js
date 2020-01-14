c2bc6f565fdc42fcf11910019d3bdc89895472c2
Howard Tong <howardtong@BDS-C02XQ51VJG5J.local>
2020-01-14 10:37:34 -0800

    Pulls dev with db dump conflicts

--- src/clincoded/static/libs/sop.js
+++ src/clincoded/static/libs/sop.js
@@ -1,4 +1,5 @@
 'use strict';
+import _ from 'underscore';
 
 /**
  * Method to determine the ClinGen SOP version of the provided evidence scoring (from a classification)
@@ -23,3 +24,26 @@ export function isScoringForCurrentSOP(classificationPoints) {
         return false;
     }
 }
+
+/**
+ * Method to determine the ClinGen SOP version from the provisional object
+ * @param {object} provisional - Object containing classification info
+*/
+export function determineSOPVersion(provisional) {
+    const sopCurrentVersion = '7';
+    if (!_.isEmpty(provisional)) {
+        if (provisional.hasOwnProperty('sopVersion')) {
+            if (provisional.sopVersion) {
+                return provisional.sopVersion;
+            } else {
+                // Until "current" classification is approved (where SOP is selected), assume curation is taking place under current SOP
+                return sopCurrentVersion;
+            }
+        } else {
+            // For classifications saved before users could select an SOP version, use existing determination logic
+            return sopVersionByScoring(provisional.classificationPoints);
+        }
+    }
+}
+
+export const sopVersions = ['7', '6', '5', '4', '3', '2', '1'];
